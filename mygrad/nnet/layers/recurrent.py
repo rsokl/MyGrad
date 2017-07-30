c2edcdd37aa8eb7ae7113ecb18f8d6589cc8d095
Ryan Soklaski <rsoklaski@gmail.com>
2017-07-30 17:14:00 -0400

    RNN backprop optimization passes tests!

--- mygrad/nnet/layers/recurrent.py
+++ mygrad/nnet/layers/recurrent.py
@@ -6,9 +6,10 @@ from numba import njit
 
 
 @njit
-def _dot_accum(x, W):
+def _dot_tanh_accum(x, W):
     for n in range(len(x) - 1):
         x[n + 1] += np.dot(x[n], W)
+        x[n + 1] = np.tanh(x[n + 1])
 
 
 class RecurrentUnit(Operation):
@@ -32,12 +33,66 @@ class RecurrentUnit(Operation):
         if self._hidden_seq:
             out[0] = self._hidden_seq[-1].data
         elif s0 is not None:
+            out[0] = s0.data if isinstance(s0, Tensor) else s0
+
+        np.dot(seq, self.U.data, out=out[1:])
+        _dot_tanh_accum(out, self.W.data)
+
+        if not self._hidden_seq:
+            self._hidden_seq = Tensor(out, _creator=self)
+        else:
+            new_dat = np.vstack((self._hidden_seq.data, out[1:]))
+            self._hidden_seq = Tensor(new_dat, _creator=self)
+
+        return self._hidden_seq
+
+
+    def backward(self, grad, seq_index=None):
+        s = self._hidden_seq
+
+        dsdf = (1 - s.data ** 2)
+        grad = grad * dsdf
+        old_grad = np.zeros_like(grad)
+        for i in range(min(s.shape[0] - 1, self.bp_lim)):
+            dt = grad[2:len(grad) - i] - old_grad[2:len(grad) - i]
+            old_grad = np.copy(grad)
+            grad[1:len(grad) - (i + 1)] += dsdf[1:len(grad) - (i + 1)] * np.dot(dt, self.W.data.T)
+
+        self.U.backward(np.einsum("ijk, ijl -> kl", self._input_seq, grad[1:]))
+        self.W.backward(np.einsum("ijk, ijl -> kl", s.data[:-1], grad[1:]))
+
+
+
+class OldRecurrentUnit(Operation):
+    def __init__(self, U, W, V, bp_lim):
+        self.U = U
+        self.W = W
+        self.V = V
+        self.bp_lim = bp_lim
+
+        self._input_seq = None
+        self._hidden_seq = []
+
+        self.bp_cnt = 0
+
+    def __call__(self, seq, s0=None):
+
+        if self._input_seq is None:
+            self._input_seq = seq
+        else:
+            self._input_seq = np.vstack((self._input_seq, seq))
+
+        out = np.zeros((seq.shape[0] + 1, seq.shape[1], self.U.shape[-1]))
+
+        if self._hidden_seq:
+            out[0] = self._hidden_seq[-1].data
+        elif s0 is not None:
             out[0] = s0 if isinstance(s0, Tensor) else s0
 
         np.dot(seq, self.U.data, out=out[1:])
-        _dot_accum(out, self.W.data)
-
-        np.tanh(out, out=out)
+        for n, s_prev in enumerate(out[:-1]):
+            out[n + 1] += s_prev.dot(self.W.data)
+            out[n + 1] = np.tanh(out[n + 1])
 
         if not self._hidden_seq:
             self._hidden_seq = [Tensor(out[0], _creator=self, _seq_index=0)]
@@ -66,19 +121,6 @@ class RecurrentUnit(Operation):
         else:
             s_prev.grad = None
             s_prev.backward(np.dot(dLdo, self.W.data.T))
-
-    def backward(self, grad):
-        s = self._hidden_seq
-        dsdf = (1 - s.data ** 2)
-        for i in range(self.bp_cnt):
-            grad[1:len(grad)-(i + 1)] += np.dot(grad[2:len(grad) - i] * dsdf[2:len(grad) - i], self.W.data.T)
-        self.U.backward(np.einsum("ijk,ijl -> kl", self._input_seq, grad[1:]))
-        self.W.backward(np.einsum("ijk,ijl -> kl", s[:-1], grad[1:]))
-
-
-def recurrent(s, x):
-    return Tensor._op(RecurrentUnit, s, op_kwargs={"seq": x})
-
 
 
 
