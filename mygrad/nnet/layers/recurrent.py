69c3479da58fda0152ef4ae7f295ef4d5e3f92ec
Ryan Soklaski <rsoklaski@gmail.com>
2017-08-13 12:43:55 -0400

    Simplified/optimized simple-rnn. Removed unused 'seq_index' from base-tensor. All tests passing

--- mygrad/nnet/layers/recurrent.py
+++ mygrad/nnet/layers/recurrent.py
@@ -25,7 +25,7 @@ class RecurrentUnit(Operation):
         For a language model, S_{t} is traditionally mapped to a prediction of X_t via: softmax(V S_t),
         where V is yet another trainable parameter (not built into the recurrent unit)."""
 
-    def __call__(self, X, U, W, s0=None, bp_lim=None, backprop_s=False):
+    def __call__(self, X, U, W, s0=None, bp_lim=None):
         """ Performs a forward pass of sequential data through a simple RNN layer, returning
             the 'hidden-descriptors' arrived at by utilizing the trainable parameters U and V:
 
@@ -50,18 +50,14 @@ class RecurrentUnit(Operation):
                 The (non-zero) limit of the number of back propagations through time are
                 performed
 
-            backprop_s : bool, (default=True)
-                If False, backpropagation will not be carried through the hidden-descriptors through
-                this RNN.
 
             Returns
             -------
             mygrad.Tensor
                 The sequence of 'hidden-descriptors' produced by the forward pass of the RNN."""
         if bp_lim is not None:
-            assert isinstance(bp_lim, Integral) and bp_lim > 0
+            assert isinstance(bp_lim, Integral) and 0 < bp_lim <= len(X)
         self.bp_lim = bp_lim if bp_lim is not None else len(X)
-        self.backprop_s = backprop_s
 
         self.X = X
         self.U = U
@@ -81,40 +77,38 @@ class RecurrentUnit(Operation):
 
         return self._hidden_seq.data
 
-    def backward(self, grad, seq_index=None):
-        if self.U.constant and self.W.constant and self.X.constant and self.backprop_s:
+    def backward(self, grad):
+        """ Performs back propagation through time (with optional truncation), using the
+            following notation:
+                s_t = tanh(f_t)
+                f_t = U x_{t-1} + W s_{t-1}
+        """
+        if self.U.constant and self.W.constant and self.X.constant:
             return None
 
         s = self._hidden_seq
 
-        dst_dft = (1 - s.data ** 2)
+        dst_dft = (1 - s.data ** 2)  # ds_{t} / d_f{t}
+        dLt_dst = np.copy(grad)  # dL_{t} / ds_{t}
+        old_dst = np.zeros_like(grad)
 
-        if self.backprop_s:
-            dLt_dst = grad * 1  # dLt / dst
-            old_dst = np.zeros_like(grad)
+        for i in range(self.bp_lim):
+            # dL_{n} / ds_{t+1} -> dL_{n} / df_{t+1}  | ( n > t )
+            index = slice(2, len(grad) - i)
+            dLn_ft1 = dst_dft[index] * (dLt_dst[index] - old_dst[index])
+            old_dst = np.copy(dLt_dst)
+            dLt_dst[1:len(grad) - (i + 1)] += np.dot(dLn_ft1, self.W.data.T)  # dL_{t} / ds_{t} + ... + dL_{n} / ds_{t}
 
-        dLt_dft = grad * dst_dft  # dLt / dst
-        old_dft = np.zeros_like(grad)
+        self._hidden_seq.grad = dLt_dst  # element t: dL_{t} / ds_{t} + ... + dL_{T_lim} / ds_{t}
 
-        if self.backprop_s:
-            for i in range(min(s.shape[0] - 1, self.bp_lim)):
-                dst = dst_dft[2:len(grad) - i] * (dLt_dst[2:len(grad) - i] - old_dst[2:len(grad) - i])  # ds_t+1 / df_t
-                old_dst = np.copy(dLt_dst)
-                dLt_dst[1:len(grad) - (i + 1)] += np.dot(dst, self.W.data.T)  # ds_t+1 / ds_t
+        dLt_dft = dLt_dst[1:] * dst_dft[1:]  # dL_{t} / df_{t} + ... + dL_{T_lim} / df_{t}
 
-        for i in range(min(s.shape[0] - 1, self.bp_lim)):
-            dft = dLt_dft[2:len(grad) - i] - old_dft[2:len(grad) - i]
-            old_dft = np.copy(dLt_dft)
-            dLt_dft[1:len(grad) - (i + 1)] += dst_dft[1:len(grad) - (i + 1)] * np.dot(dft, self.W.data.T)
-
-        if self.backprop_s:
-            self._hidden_seq.grad = dLt_dst
         if not self.U.constant:
-            self.U.backward(np.einsum("ijk, ijl -> kl", self.X.data, dLt_dft[1:]))
+            self.U.backward(np.einsum("ijk, ijl -> kl", self.X.data, dLt_dft))  # dL_{1} / dU + ... + dL_{T} / dU
         if not self.W.constant:
-            self.W.backward(np.einsum("ijk, ijl -> kl", s.data[:-1], dLt_dft[1:]))
+            self.W.backward(np.einsum("ijk, ijl -> kl", s.data[:-1], dLt_dft))  # dL_{1} / dW + ... + dL_{T} / dW
         if not self.X.constant:
-            self.X.backward(np.dot(dLt_dft[1:], self.U.data.T))
+            self.X.backward(np.dot(dLt_dft, self.U.data.T))  # dL_{1} / dX + ... + dL_{T} / dX
 
     def null_gradients(self):
         """ Back-propagates `None` to the gradients of the operation's input Tensors."""
@@ -122,7 +116,7 @@ class RecurrentUnit(Operation):
             x.null_gradients()
 
 
-def simple_RNN(X, U, W, s0=None, bp_lim=None, backprop_s=False):
+def simple_RNN(X, U, W, s0=None, bp_lim=None):
     """ Performs a forward pass of sequential data through a simple RNN layer, returning
         the 'hidden-descriptors' arrived at by utilizing the trainable parameters U and V:
 
@@ -163,6 +157,6 @@ def simple_RNN(X, U, W, s0=None, bp_lim=None, backprop_s=False):
         N : Batch size
         C : Length of single datum
         D : Length of 'hidden' descriptor"""
-    s = Tensor._op(RecurrentUnit, X, U, W, op_kwargs=dict(s0=s0, bp_lim=bp_lim, backprop_s=backprop_s))
+    s = Tensor._op(RecurrentUnit, X, U, W, op_kwargs=dict(s0=s0, bp_lim=bp_lim))
     s.creator._hidden_seq = s
     return s
