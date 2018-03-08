05ef71d556730f1ae2dd27abe39b658c970970bb
Ryan Soklaski <rsoklaski@gmail.com>
2018-03-08 17:41:49 -0500

    udate docs, remove old pooling

--- mygrad/nnet/layers/pooling.py
+++ mygrad/nnet/layers/pooling.py
@@ -32,7 +32,7 @@ class MaxPoolND(Operation):
             Notes
             -----
             Only 'valid' placements of the pooling window are permitted - the pooling
-            window cannot extend passed the "boundaries" of the data in the H and W
+            window cannot extend passed the "boundaries" of the data
             dimensions.
             """
         self.a = x  # data: ((N0, ...), C0, ...)
@@ -86,165 +86,65 @@ class MaxPoolND(Operation):
         grid_shape = sl.shape
         maxed = sl.reshape(*sl.shape[:-num_pool], -1).argmax(-1)
         axes = tuple(range(maxed.ndim))
+
+        # argmax within a given flat-window
         maxed = maxed.transpose(axes[num_pool:] + axes[:num_pool])  # ((N0, ...), G0, ...)
 
+        # flat-index offset associated with reshaped window within `x`
         row_major_offset = tuple(np.cumprod(x.shape[-num_pool:][:0:-1])[::-1]) + (1,)
 
-        # i*W + j
+        # flat index of argmax, updated based on position within window, according to shape of `x`
         in_window_offset = sum(
             ind * off for ind, off in zip(np.unravel_index(maxed, self.pool),
                                           row_major_offset))
 
-        # ih*stride[0]*W + jh*stride[1]
+        # flat-index of strided window placement, relative to `x`
         window_offset = sum(ind * s * off for ind, s, off in zip(np.indices(grid_shape[:num_pool]),
                                                                  self.stride,
                                                                  row_major_offset))
 
+        # indices required to traverse pool-axis-flattened array
         # ((N0, ...) G0*...)
         flat_grid_shape = (*maxed.shape[:-num_pool], np.prod(maxed.shape[-num_pool:]))
         index = np.indices(flat_grid_shape)
+
+        # update trailing indices to traverse location of max entries within pooled axes
         index[-1] = (in_window_offset + window_offset).reshape(*flat_grid_shape[:-1], -1)
 
+        # accumulate gradient within pool-axis-flattened dx, then reshape to match shape of `x`
         dx = np.zeros(x.shape[:-num_pool] + (np.prod(x.shape[-num_pool:]),))
         np.add.at(dx, tuple(index), grad.reshape(*x.shape[:-num_pool], -1))
         self.a.backward(dx.reshape(x.shape))
 
 
 def max_pool(x, pool, stride):
-    """ Perform max-pooling over the last two dimensions of a data batch.
+    """ Perform max-pooling over the last N dimensions of a data batch.
 
         Parameters
         ----------
-        x : Tensor, shape=(N, C, H, W)
+        x : mygrad.Tensor, shape=([(N0, ...), C0, ...])
             The data batch; to be pooled along the H and W axes
 
-        pool : Union[int, Tuple[int, int]]
-            The extent of the pooling window along the H and W axes, respectively. If
-            a single value is provided, it is used for both axes.
+        pool : Tuple[Integral, ...]
+            (P0, ...)
 
-        stride : Union[int, Tuple[int, int]]
-            The spacing used to place the pooling window, along the H and W axes, respectively.
-            If a single value is provided, it is used for both axes.
+            The extent of the pooling window along the (C0, ...) axes, respectively. The
+            length of `pool` determines N - the number of trailing dimensions to pool over.
+
+        stride : Union[Integral, Tuple[Integral, ...]]
+            The spacing used to place the pooling window, along (P0, ...) axes, respectively.
+            If a single value is provided, it is used for all N pooling axes.
 
         Returns
         -------
-        Tensor, shape=(N, C, H', W')
+        numpy.ndarray, shape=((N0, ...), C0', ...)
             The pooled data batch.
 
         Notes
         -----
         Only 'valid' placements of the pooling window are permitted - the pooling
-        window may not extend passed the "boundaries" of the data in the H and W
+        window cannot extend passed the "boundaries" of the data
         dimensions.
-
-        This is a memory-intensive implementation of max pooling. """
+        """
     if isinstance(pool, Integral): pool = (pool, pool)
     return Tensor._op(MaxPoolND, x, op_args=(pool, stride))
-
-
-class MaxPoolOld(Operation):
-    def __call__(self, x, pool, stride):
-        """ Perform max-pooling over the last two dimensions of a data batch.
-
-            Parameters
-            ----------
-            x : Tensor, shape=(N, C, H, W)
-                The data batch; to be pooled along the H and W axes
-
-            pool : Union[int, Tuple[int, int]]
-                The extent of the pooling window along the H and W axes, respectively. If
-                a single value is provided, it is used for both axes.
-
-            stride : Union[int, Tuple[int, int]]
-                The spacing used to place the pooling window, along the H and W axes, respectively.
-                If a single value is provided, it is used for both axes.
-
-            Returns
-            -------
-            numpy.ndarray, shape=(N, C, H', W')
-                The pooled data batch.
-
-            Notes
-            -----
-            Only 'valid' placements of the pooling window are permitted - the pooling
-            window cannot extend passed the "boundaries" of the data in the H and W
-            dimensions.
-            """
-        self.a = x  # data: (N, C, H, W)
-        x = x.data
-
-        assert x.ndim == 4, "The data batch must have the shape (N, C, H, W)"
-        stride = np.array((stride, stride)) if isinstance(stride, Integral) else np.asarray(stride, dtype=int)
-        assert len(stride) == 2 and np.all(stride >= 1) #and np.issubdtype(stride.dtype, np.int)
-
-        pool = np.array((pool, pool)) if isinstance(pool, Integral) else np.asarray(pool, dtype=int)
-        assert len(pool) == 2 and np.all(pool >= 1)# and np.issubdtype(pool.dtype, np.int)
-        pool = (1, 1) + tuple(pool)
-
-        x_shape = np.array(x.shape[2:])
-        w_shape = np.array(pool[2:])
-
-        out_shape = (x_shape - w_shape) / stride + 1
-
-        if not all(i.is_integer() and i > 0 for i in out_shape):
-            msg = "Stride and kernel dimensions are incompatible: \n"
-            msg += "Input dimensions: {}\n".format(tuple(x_shape))
-            msg += "Stride dimensions: {}\n".format(tuple(stride))
-            msg += "Pooling dimensions: {}\n".format(tuple(w_shape))
-            raise ValueError(msg)
-
-        n, c, ho, wo = (x.shape[0], x.shape[1]) + tuple(out_shape.astype(int))
-
-        x_reshaped = x.reshape(x.shape[0] * x.shape[1], 1, *x.shape[2:])
-        x_col = im2col(x_reshaped, pool, (n, c, ho, wo), [0, 0], stride)
-
-        max_idx = np.argmax(x_col, axis=0)
-        out = x_col[max_idx, range(max_idx.size)]
-
-        self.cache = max_idx
-        self.pool = pool
-        self.stride = stride
-        self.col_shape = x_col.shape
-        return out.reshape(ho, wo, n, c).transpose(2, 3, 0, 1)
-
-    def backward_a(self, grad):
-        max_id = self.cache
-
-        dx_col = np.zeros(self.col_shape, dtype=self.a.dtype)
-        dx_col[max_id, range(grad.size)] = grad.transpose(2, 3, 0, 1).ravel()
-        n, c, h, w = self.a.shape
-        dx = col2im(dx_col, (n * c, 1, h, w), self.pool, grad.shape, [0, 0], self.stride)
-        self.a.backward(dx.reshape(self.a.shape))
-
-
-def max_poolold(x, pool, stride):
-    """ Perform max-pooling over the last two dimensions of a data batch.
-
-        Parameters
-        ----------
-        x : Tensor, shape=(N, C, H, W)
-            The data batch; to be pooled along the H and W axes
-
-        pool : Union[int, Tuple[int, int]]
-            The extent of the pooling window along the H and W axes, respectively. If
-            a single value is provided, it is used for both axes.
-
-        stride : Union[int, Tuple[int, int]]
-            The spacing used to place the pooling window, along the H and W axes, respectively.
-            If a single value is provided, it is used for both axes.
-
-        Returns
-        -------
-        Tensor, shape=(N, C, H', W')
-            The pooled data batch.
-
-        Notes
-        -----
-        Only 'valid' placements of the pooling window are permitted - the pooling
-        window may not extend passed the "boundaries" of the data in the H and W
-        dimensions.
-
-        This is a memory-intensive implementation of max pooling. """
-    return Tensor._op(MaxPoolOld, x, op_args=(pool, stride))
-
-
