fix bounds error in backprop test factory; renam params; inverse hyper trig NOT passing

tests/math/test_abs.py
@@ -3,9 +3,9 @@ from mygrad.math import abs
 import numpy as np
 
 
-@fwdprop_test_factory(mygrad_op=abs, true_func=np.abs)
+@fwdprop_test_factory(mygrad_func=abs, true_func=np.abs)
 def test_abs_fwd(): pass
 
 
-@backprop_test_factory(mygrad_op=abs, xbnds=(-100, 100), no_go=(0,))
+@backprop_test_factory(mygrad_func=abs, xbnds=(-100, 100), no_go=(0,))
 def test_abs_backward(): pass

tests/math/test_cbrt.py
@@ -3,9 +3,9 @@ from mygrad.math import cbrt
 import numpy as np
 
 
-@fwdprop_test_factory(mygrad_op=cbrt, true_func=np.cbrt)
+@fwdprop_test_factory(mygrad_func=cbrt, true_func=np.cbrt)
 def test_cbrt_fwd(): pass
 
 
-@backprop_test_factory(mygrad_op=cbrt, xbnds=(-100, 100), no_go=(0,))
+@backprop_test_factory(mygrad_func=cbrt, xbnds=(-100, 100), no_go=(0,))
 def test_cbrt_backward(): pass

tests/math/test_hyperbolictrig.py
@@ -4,49 +4,49 @@ from mygrad.math import sinh, cosh, tanh, csch, sech, coth
 import numpy as np
 
 
-@fwdprop_test_factory(mygrad_op=sinh, true_func=np.sinh, xbnds=(-10, 10))
+@fwdprop_test_factory(mygrad_func=sinh, true_func=np.sinh, xbnds=(-10, 10))
 def test_sinh_fwd(): pass
 
 
-@backprop_test_factory(mygrad_op=sinh, xbnds=(-10, 10))
+@backprop_test_factory(mygrad_func=sinh, xbnds=(-10, 10))
 def test_sinh_backward(): pass
 
 
-@fwdprop_test_factory(mygrad_op=cosh, true_func=np.cosh, xbnds=(-10, 10))
+@fwdprop_test_factory(mygrad_func=cosh, true_func=np.cosh, xbnds=(-10, 10))
 def test_cosh_fwd(): pass
 
 
-@backprop_test_factory(mygrad_op=cosh, xbnds=(-10, 10), atol=1e-5)
+@backprop_test_factory(mygrad_func=cosh, xbnds=(-10, 10), atol=1e-5)
 def test_cosh_backward(): pass
 
 
-@fwdprop_test_factory(mygrad_op=tanh, true_func=np.tanh, xbnds=(-10, 10))
+@fwdprop_test_factory(mygrad_func=tanh, true_func=np.tanh, xbnds=(-10, 10))
 def test_tanh_fwd(): pass
 
 
-@backprop_test_factory(mygrad_op=tanh, xbnds=(-10, 10), atol=1e-5)
+@backprop_test_factory(mygrad_func=tanh, xbnds=(-10, 10), atol=1e-5)
 def test_tanh_backward(): pass
 
 
-@fwdprop_test_factory(mygrad_op=csch, true_func=lambda x: 1 / np.sinh(x), xbnds=(.001, 10))
+@fwdprop_test_factory(mygrad_func=csch, true_func=lambda x: 1 / np.sinh(x), xbnds=(.001, 10))
 def test_csch_fwd(): pass
 
 
-@backprop_test_factory(mygrad_op=csch, xbnds=(.001, 10))
+@backprop_test_factory(mygrad_func=csch, xbnds=(.001, 10))
 def test_csch_backward(): pass
 
 
-@fwdprop_test_factory(mygrad_op=sech, true_func=lambda x: 1 / np.cosh(x), xbnds=(-10, 10))
+@fwdprop_test_factory(mygrad_func=sech, true_func=lambda x: 1 / np.cosh(x), xbnds=(-10, 10))
 def test_sech_fwd(): pass
 
 
-@backprop_test_factory(mygrad_op=sech, xbnds=(.001, 10), atol=1e-5)
+@backprop_test_factory(mygrad_func=sech, xbnds=(.001, 10), atol=1e-5)
 def test_sech_backward(): pass
 
 
-@fwdprop_test_factory(mygrad_op=coth, true_func=lambda x: 1 / np.tanh(x), xbnds=(-10, 10), no_go=(0,))
+@fwdprop_test_factory(mygrad_func=coth, true_func=lambda x: 1 / np.tanh(x), xbnds=(-10, 10), no_go=(0,))
 def test_coth_fwd(): pass
 
 
-@backprop_test_factory(mygrad_op=coth, xbnds=(.001, 10), atol=1e-5)
+@backprop_test_factory(mygrad_func=coth, xbnds=(.001, 10), atol=1e-5)
 def test_coth_backward(): pass

tests/math/test_inversehyperbolictrig.py
@@ -1,182 +1,51 @@
-from mygrad.tensor_base import Tensor
+from ..wrappers.unary_func import fwdprop_test_factory, backprop_test_factory
 from mygrad.math import arcsinh, arccosh, arctanh, arccsch, arcsech, arccoth
-
-import hypothesis.strategies as st
-from hypothesis import given
-import hypothesis.extra.numpy as hnp
-
 import numpy as np
 
 
-@given(st.data())
-def test_arcsinh_fwd(data):
-    a = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 10)))
-
-    result = np.arcsinh(a)
-
-    o = arcsinh(a)
-    assert isinstance(o, Tensor)
-    assert np.allclose(o.data, result)
+@fwdprop_test_factory(mygrad_func=arcsinh, true_func=np.arcsinh)
+def test_arcsinh_fwd(): pass
 
 
-@given(st.data())
-def test_arcsinh_backward(data):
-    x = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 100)))
-
-    grad = data.draw(hnp.arrays(shape=x.shape,
-                                dtype=float,
-                                elements=st.floats(-10, 10)))
-
-    a = Tensor(x)
-    c = arcsinh(a)
-    c.backward(grad)
-    assert np.allclose(a.grad, grad / np.sqrt(1 + x ** 2))
+@backprop_test_factory(mygrad_func=arcsinh)
+def test_arcsinh_backward(): pass
 
 
-@given(st.data())
-def test_arccosh_fwd(data):
-    a = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(1.1, 10)))
-
-    result = np.arccosh(a)
-
-    o = arccosh(a)
-    assert isinstance(o, Tensor)
-    assert np.allclose(o.data, result)
+@fwdprop_test_factory(mygrad_func=arccosh, true_func=np.arccosh, xbnds=(1.001, 10))
+def test_arccosh_fwd(): pass
 
 
-@given(st.data())
-def test_arccosh_backward(data):
-    x = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(1.1, 100)))
-
-    grad = data.draw(hnp.arrays(shape=x.shape,
-                                dtype=float,
-                                elements=st.floats(-10, 10)))
-
-    a = Tensor(x)
-    c = arccosh(a)
-    c.backward(grad)
-    assert np.allclose(a.grad, grad / np.sqrt(x ** 2 - 1))
+@backprop_test_factory(mygrad_func=arccosh,  xbnds=(1.001, 10))
+def test_arccosh_backward(): pass
 
 
-@given(st.data())
-def test_arctanh_fwd(data):
-    a = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(-0.9, 0.9)))
-
-    result = np.arctanh(a)
-
-    o = arctanh(a)
-    assert isinstance(o, Tensor)
-    assert np.allclose(o.data, result)
+@fwdprop_test_factory(mygrad_func=arctanh, true_func=np.arctanh, xbnds=(-.5, .5))
+def test_arctanh_fwd(): pass
 
 
-@given(st.data())
-def test_arctanh_backward(data):
-    x = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(-0.9, 0.9)))
-
-    grad = data.draw(hnp.arrays(shape=x.shape,
-                                dtype=float,
-                                elements=st.floats(-10, 10)))
-
-    a = Tensor(x)
-    c = arctanh(a)
-    c.backward(grad)
-    assert np.allclose(a.grad, grad / (1 - x ** 2))
+@backprop_test_factory(mygrad_func=arctanh,  xbnds=(-0.5, 0,5), no_go=(1,))
+def test_arctanh_backward(): pass
 
 
-@given(st.data())
-def test_arccsch_fwd(data):
-    a = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 10)))
-
-    result = np.arcsinh(1 / a)
-
-    o = arccsch(a)
-    assert isinstance(o, Tensor)
-    assert np.allclose(o.data, result)
+@fwdprop_test_factory(mygrad_func=arccsch, true_func=lambda x: 1 / np.arcsinh(x), xbnds=(0.1, 10))
+def test_arccsch_fwd(): pass
 
 
-@given(st.data())
-def test_arccsch_backward(data):
-    x = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 100)))
-
-    grad = data.draw(hnp.arrays(shape=x.shape,
-                                dtype=float,
-                                elements=st.floats(-10, 10)))
-
-    a = Tensor(x)
-    c = arccsch(a)
-    c.backward(grad)
-    assert np.allclose(a.grad, -grad / (np.abs(x) * np.sqrt(1 + x ** 2)))
+@backprop_test_factory(mygrad_func=arccsch, xbnds=(0.1, 10))
+def test_arccsch_backward(): pass
 
 
-@given(st.data())
-def test_arcsech_fwd(data):
-    a = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 0.9)))
-
-    result = np.arccosh(1 / a)
-
-    o = arcsech(a)
-    assert isinstance(o, Tensor)
-    assert np.allclose(o.data, result)
+@fwdprop_test_factory(mygrad_func=arcsech, true_func=lambda x: 1 / np.arccosh(x), xbnds=(1.5, 10))
+def test_arcsech_fwd(): pass
 
 
-@given(st.data())
-def test_arcsech_backward(data):
-    x = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 0.9)))
-
-    grad = data.draw(hnp.arrays(shape=x.shape,
-                                dtype=float,
-                                elements=st.floats(-10, 10)))
-
-    a = Tensor(x)
-    c = arcsech(a)
-    c.backward(grad)
-    assert np.allclose(a.grad, -grad / (x * np.sqrt(1 - x ** 2)))
+@backprop_test_factory(mygrad_func=arcsech,  xbnds=(1.5, 10))
+def test_arcsech_backward(): pass
 
 
-@given(st.data())
-def test_arccoth_fwd(data):
-    a = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(-10, -1.1)))
-
-    result = np.arctanh(1 / a)
-
-    o = arccoth(a)
-    assert isinstance(o, Tensor)
-    assert np.allclose(o.data, result)
+@fwdprop_test_factory(mygrad_func=arccoth, true_func=lambda x: 1 / np.arctanh(x), xbnds=(1.5, 10))
+def test_arccoth_fwd(): pass
 
 
-@given(st.data())
-def test_arccoth_backward(data):
-    x = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(1.1, 100)))
-
-    grad = data.draw(hnp.arrays(shape=x.shape,
-                                dtype=float,
-                                elements=st.floats(-10, 10)))
-
-    a = Tensor(x)
-    c = arccoth(a)
-    c.backward(grad)
-    assert np.allclose(a.grad, grad / (1 - x ** 2))
+@backprop_test_factory(mygrad_func=arccoth,  xbnds=(1.5, 10))
+def test_arccoth_backward(): pass

tests/math/test_log.py
@@ -4,25 +4,25 @@ from ..wrappers.unary_func import backprop_test_factory, fwdprop_test_factory
 import numpy as np
 
 
-@fwdprop_test_factory(mygrad_op=log, true_func=np.log, xbnds=[0, 100], no_go=(0,))
+@fwdprop_test_factory(mygrad_func=log, true_func=np.log, xbnds=[0, 100], no_go=(0,))
 def test_log_fwd(): pass
 
 
-@backprop_test_factory(mygrad_op=log, xbnds=[0, 100], no_go=(0,))
+@backprop_test_factory(mygrad_func=log, xbnds=[0, 100], no_go=(0,))
 def test_log_backward(): pass
 
 
-@fwdprop_test_factory(mygrad_op=log2, true_func=np.log2, xbnds=[0, 100], no_go=(0,))
+@fwdprop_test_factory(mygrad_func=log2, true_func=np.log2, xbnds=[0, 100], no_go=(0,))
 def test_log2_fwd(): pass
 
 
-@backprop_test_factory(mygrad_op=log2, xbnds=[0, 100], no_go=(0,))
+@backprop_test_factory(mygrad_func=log2, xbnds=[0, 100], no_go=(0,))
 def test_log2_backward(): pass
 
 
-@fwdprop_test_factory(mygrad_op=log10, true_func=np.log10, xbnds=[0, 100], no_go=(0,))
+@fwdprop_test_factory(mygrad_func=log10, true_func=np.log10, xbnds=[0, 100], no_go=(0,))
 def test_log10_fwd(): pass
 
 
-@backprop_test_factory(mygrad_op=log10, xbnds=[0, 100], no_go=(0,))
+@backprop_test_factory(mygrad_func=log10, xbnds=[0, 100], no_go=(0,))
 def test_log10_backward(): pass

tests/math/test_sqrt.py
@@ -3,9 +3,9 @@ from mygrad.math import sqrt
 import numpy as np
 
 
-@fwdprop_test_factory(mygrad_op=sqrt, true_func=np.sqrt, xbnds=(0, 100))
+@fwdprop_test_factory(mygrad_func=sqrt, true_func=np.sqrt, xbnds=(0, 100))
 def test_sqrt_fwd(): pass
 
 
-@backprop_test_factory(mygrad_op=sqrt, true_func=np.sqrt, xbnds=(0, 100), no_go=(0,), atol=1e-3)
+@backprop_test_factory(mygrad_func=sqrt, true_func=np.sqrt, xbnds=(0, 100), no_go=(0,), atol=1e-3)
 def test_sqrt_backward(): pass