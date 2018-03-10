fix logaddexp backprop + optimize; implement exp; check logaddexp against explicity log(exp(a) + exp(b)) in mygrad

mygrad/operations/logaddexp.py
@@ -16,17 +16,17 @@ class Logaddexp(BroadcastableOp):
             out : numpy.ndarray """
         self.a = a
         self.b = b
-        out = np.log(np.exp(a.data) + np.exp(b.data))
+        out = np.logaddexp(a.data, b.data)
 
         self.broadcast_check(a, b, out.shape)
         return out
 
     def backward_a(self, grad):
-        dLda = grad * self.a.data / (np.exp(self.a.data) + np.exp(self.b.data))
+        dLda = grad / (1 + np.exp(self.b.data - self.a.data))
         broadcasted_grad = super(Logaddexp, self).backward_a(dLda)
         self.a.backward(broadcasted_grad)
 
     def backward_b(self, grad):
-        dLdb = grad * self.b.data / (np.exp(self.a.data) + np.exp(self.b.data))
+        dLdb = grad / (1 + np.exp(self.a.data - self.b.data))
         broadcasted_grad = super(Logaddexp, self).backward_b(dLdb)
         self.b.backward(broadcasted_grad)