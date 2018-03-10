653a0cc42670041795b2f42ec5f1a6b8c224cccb
Ryan Soklaski <rsoklaski@gmail.com>
2018-03-10 16:14:44 -0500

    wrapper ->cube-root

--- tests/math/test_cbrt.py
+++ tests/math/test_cbrt.py
@@ -1,37 +1,11 @@
-from mygrad.tensor_base import Tensor
+from ..wrappers.unary_func import fwdprop_test, backprop_test
 from mygrad.math import cbrt
-
-import hypothesis.strategies as st
-from hypothesis import given
-import hypothesis.extra.numpy as hnp
-
 import numpy as np
 
 
-@given(st.data())
-def test_cbrt_fwd(data):
-    a = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 10)))
-
-    result = np.cbrt(a)
-
-    o = cbrt(a)
-    assert isinstance(o, Tensor)
-    assert np.allclose(o.data, result)
+@fwdprop_test(mygrad_op=cbrt, true_func=np.cbrt)
+def test_cbrt_fwd(): pass
 
 
-@given(st.data())
-def test_cbrt_backward(data):
-    x = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 100)))
-
-    grad = data.draw(hnp.arrays(shape=x.shape,
-                                dtype=float,
-                                elements=st.floats(-10, 10)))
-
-    a = Tensor(x)
-    c = cbrt(a)
-    c.backward(grad)
-    assert np.allclose(a.grad, grad / (3 * np.cbrt(x ** 2)))
+@backprop_test(mygrad_op=cbrt, xbnds=(-100, 100), no_go=(0,))
+def test_cbrt_backward(): pass
