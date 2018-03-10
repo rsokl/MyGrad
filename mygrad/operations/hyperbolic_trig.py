fix coth backprop; use wrappers

mygrad/operations/hyperbolic_trig.py
@@ -38,8 +38,6 @@ class Tanh(Operation):
 class Csch(Operation):
     def __call__(self, a):
         self.a = a
-        if np.any(a == 0):
-            raise ValueError("Invalid csch-domain value")
         return 1 / np.sinh(a.data)
 
     def backward_a(self, grad):
@@ -58,9 +56,7 @@ class Sech(Operation):
 class Coth(Operation):
     def __call__(self, a):
         self.a = a
-        if np.any(a == 0):
-            raise ValueError("Invalid coth-domain value")
         return 1 / np.tanh(a.data)
 
     def backward_a(self, grad):
-        return self.a.backward(grad - np.cosh(self.a.data)**2 / np.sinh(self.a.data)**2)
+        return self.a.backward(grad * -1 / np.sinh(self.a.data)**2)

tests/math/test_hyperbolictrig.py
@@ -1,182 +1,52 @@
-from mygrad.tensor_base import Tensor
+from ..wrappers.unary_func import fwdprop_test, backprop_test
 from mygrad.math import sinh, cosh, tanh, csch, sech, coth
-
-import hypothesis.strategies as st
-from hypothesis import given
-import hypothesis.extra.numpy as hnp
 
 import numpy as np
 
 
-@given(st.data())
-def test_sinh_fwd(data):
-    a = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 10)))
-
-    result = np.sinh(a)
-
-    o = sinh(a)
-    assert isinstance(o, Tensor)
-    assert np.allclose(o.data, result)
+@fwdprop_test(mygrad_op=sinh, true_func=np.sinh,  xbnds=(-10, 10))
+def test_sinh_fwd(): pass
 
 
-@given(st.data())
-def test_sinh_backward(data):
-    x = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 100)))
-
-    grad = data.draw(hnp.arrays(shape=x.shape,
-                                dtype=float,
-                                elements=st.floats(-10, 10)))
-
-    a = Tensor(x)
-    c = sinh(a)
-    c.backward(grad)
-    assert np.allclose(a.grad, grad * np.cosh(x))
+@backprop_test(mygrad_op=sinh, xbnds=(-10, 10))
+def test_sinh_backward(): pass
 
 
-@given(st.data())
-def test_cosh_fwd(data):
-    a = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 10)))
-
-    result = np.cosh(a)
-
-    o = cosh(a)
-    assert isinstance(o, Tensor)
-    assert np.allclose(o.data, result)
+@fwdprop_test(mygrad_op=cosh, true_func=np.cosh,  xbnds=(-10, 10))
+def test_cosh_fwd(): pass
 
 
-@given(st.data())
-def test_cosh_backward(data):
-    x = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 100)))
-
-    grad = data.draw(hnp.arrays(shape=x.shape,
-                                dtype=float,
-                                elements=st.floats(-10, 10)))
-
-    a = Tensor(x)
-    c = cosh(a)
-    c.backward(grad)
-    assert np.allclose(a.grad, grad * np.sinh(x))
+@backprop_test(mygrad_op=cosh, xbnds=(-10, 10), atol=1e-5)
+def test_cosh_backward(): pass
 
 
-@given(st.data())
-def test_tanh_fwd(data):
-    a = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 10)))
-
-    result = np.tanh(a)
-
-    o = tanh(a)
-    assert isinstance(o, Tensor)
-    assert np.allclose(o.data, result)
+@fwdprop_test(mygrad_op=tanh, true_func=np.tanh,  xbnds=(-10, 10))
+def test_tanh_fwd(): pass
 
 
-@given(st.data())
-def test_tanh_backward(data):
-    x = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 100)))
-
-    grad = data.draw(hnp.arrays(shape=x.shape,
-                                dtype=float,
-                                elements=st.floats(-10, 10)))
-
-    a = Tensor(x)
-    c = tanh(a)
-    c.backward(grad)
-    assert np.allclose(a.grad, grad * (1 - np.tanh(x) ** 2))
+@backprop_test(mygrad_op=tanh, xbnds=(-10, 10), atol=1e-5)
+def test_tanh_backward(): pass
 
 
-@given(st.data())
-def test_csch_fwd(data):
-    a = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 10)))
-
-    result = 1 / np.sinh(a)
-
-    o = csch(a)
-    assert isinstance(o, Tensor)
-    assert np.allclose(o.data, result)
+@fwdprop_test(mygrad_op=csch, true_func=lambda x: 1 / np.sinh(x),  xbnds=(.001, 10))
+def test_csch_fwd(): pass
 
 
-@given(st.data())
-def test_csch_backward(data):
-    x = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 100)))
-
-    grad = data.draw(hnp.arrays(shape=x.shape,
-                                dtype=float,
-                                elements=st.floats(-10, 10)))
-
-    a = Tensor(x)
-    c = csch(a)
-    c.backward(grad)
-    assert np.allclose(a.grad, grad * -np.cosh(x) / np.sinh(x) ** 2)
+@backprop_test(mygrad_op=csch, xbnds=(.001, 10))
+def test_csch_backward(): pass
 
 
-@given(st.data())
-def test_sech_fwd(data):
-    a = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 10)))
-
-    result = 1 / np.cosh(a)
-
-    o = sech(a)
-    assert isinstance(o, Tensor)
-    assert np.allclose(o.data, result)
+@fwdprop_test(mygrad_op=sech, true_func=lambda x: 1 / np.cosh(x),  xbnds=(-10, 10))
+def test_sech_fwd(): pass
 
 
-@given(st.data())
-def test_sech_backward(data):
-    x = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 100)))
-
-    grad = data.draw(hnp.arrays(shape=x.shape,
-                                dtype=float,
-                                elements=st.floats(-10, 10)))
-
-    a = Tensor(x)
-    c = sech(a)
-    c.backward(grad)
-    assert np.allclose(a.grad, grad * -np.sinh(x) / np.cosh(x) ** 2)
+@backprop_test(mygrad_op=sech, xbnds=(.001, 10), atol=1e-5)
+def test_sech_backward(): pass
 
 
-@given(st.data())
-def test_coth_fwd(data):
-    a = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 10)))
-
-    result = 1 / np.tanh(a)
-
-    o = coth(a)
-    assert isinstance(o, Tensor)
-    assert np.allclose(o.data, result)
+@fwdprop_test(mygrad_op=coth, true_func=lambda x: 1 / np.tanh(x),  xbnds=(-10, 10), no_go=(0,))
+def test_coth_fwd(): pass
 
 
-@given(st.data())
-def test_coth_backward(data):
-    x = data.draw(hnp.arrays(shape=hnp.array_shapes(max_side=3, max_dims=3),
-                             dtype=float,
-                             elements=st.floats(0.1, 100)))
-
-    grad = data.draw(hnp.arrays(shape=x.shape,
-                                dtype=float,
-                                elements=st.floats(-10, 10)))
-
-    a = Tensor(x)
-    c = coth(a)
-    c.backward(grad)
-    assert np.allclose(a.grad, grad - np.cosh(x) ** 2 / np.sinh(x) ** 2)
+@backprop_test(mygrad_op=coth, xbnds=(.001, 10), atol=1e-5)
+def test_coth_backward(): pass

tests/wrappers/unary_func.py
@@ -46,7 +46,7 @@ class backprop_test():
         @given(st.data())
         @wraps(f)
         def wrapper(data):
-
+            from hypothesis import note
             # sampled x coord, and df/fx evaluated at x (numerically)
             # both are of type Decimal
             x, dx = data.draw(numerical_derivative(self.func, xbnds=[0, 100], no_go=(0,)))
@@ -58,5 +58,7 @@ class backprop_test():
             var = Tensor(float(x))
             self.op(var).backward(float(grad))
             var_grad = var.grad.item()
+            note(f"dx * grad: {float(dx * grad)}")
+            note(f"grad: {var_grad}")
             assert np.isclose(float(dx * grad), var_grad, **self.tolerances)
         return wrapper