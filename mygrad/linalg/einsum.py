fix broadcasting bug in einsum; added more broadcasting examples

mygrad/linalg/einsum.py
@@ -11,7 +11,6 @@ __all__ = ["einsum"]
 
 
 def reduce_broadcast(grad, outshape):
-
     if grad.shape == outshape:
         return grad
 
@@ -22,6 +21,7 @@ def reduce_broadcast(grad, outshape):
     keepdims = tuple(n for n,i in enumerate(grad.shape) if i != outshape[n])
     if keepdims:
         grad = grad.sum(axis=keepdims, keepdims=True)
+
     return grad
 
 
@@ -151,6 +151,10 @@ class EinSum(MultiVarBroadcastableOp):
             # dfdx: einsum("ji, k -> ijk", grad, y)
             outshape = self.variables[index].shape
             dfdx = reduce_broadcast(np.einsum(back_prop_lbls, *operands), outshape)
+            if var_shape != dfdx.shape:
+                # if y was broadcast over x, the gradient needs to
+                # be broadcast to x's shape: dfdx-shape (i,j,1) -> (i,j,k)
+                dfdx = np.broadcast_to(dfdx, var_shape)
             self.variables[index].backward(dfdx)
             return None
 

tests/linalg/test_einsum.py
@@ -171,7 +171,7 @@ def test_einsum_static_bkwd():
     compare_backprop(a, [0, 1, 2], b, [1, 0, 3], [2, 3], atol=1e-3, rtol=1e-3)
     
     a = np.arange(6).reshape((3, 2))
-    b = np.arange(12).reshape((4, 3))
+    b = np.arange(4).reshape((4, 1))
     compare_backprop('ki,jk->ij', a, b)
     compare_backprop(a, [0, 1], b, [2, 0], [1, 2])
     
@@ -185,7 +185,7 @@ def test_einsum_static_bkwd():
 def test_traces_bkwd():
     a = np.random.rand(5, 2, 2, 5)
     b = np.random.rand(3, 2, 1)
-    c = np.random.rand(2, 2)
+    c = np.random.rand(1, 1)
     compare_backprop('ijji -> i', a)
     compare_backprop(a, [0, 1, 1, 0], [0])
 