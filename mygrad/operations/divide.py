3e9c46bf93cf2d05f11157940a75c9beb904a2a6
Ryan Soklaski <rsoklaski@gmail.com>
2018-03-25 14:01:22 -0400

    add null-grad check to binary wrapper; update divide op

--- mygrad/operations/divide.py
+++ mygrad/operations/divide.py
@@ -1,18 +1,18 @@
-from .operation_base import BroadcastableOp
+from .multivar_operations import MultiVarBroadcastableOp
 import numpy as np
 
-class Divide(BroadcastableOp):
+class Divide(MultiVarBroadcastableOp):
     def __call__(self, a, b):
+        """ f(a, b) -> a / b"""
         if np.any(b == 0):
             raise ZeroDivisionError
-        self.a = a
-        self.b = b
+        self.variables = (a, b)
         out = a.data / b.data
-        self.broadcast_check(a, b, out.shape)
         return out
 
-    def backward_a(self, grad):
-        self.a.backward(super(Divide, self).backward_a(grad / self.b.data))
-
-    def backward_b(self, grad):
-        self.b.backward(super(Divide, self).backward_b(- grad * self.a.data / (self.b.data ** 2)))
+    def backward_var(self, grad, index, **kwargs):
+        a, b = self.variables
+        if index == 0:  # backprop through a
+            a.backward(grad / b.data, **kwargs)
+        else:           # broadcast through b
+            b.backward(- grad * a.data / (b.data ** 2), **kwargs)
