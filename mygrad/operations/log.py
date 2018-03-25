d952fef931cb47caeb72ee38c67e6a130111dd27
Ryan Soklaski <rsoklaski@gmail.com>
2018-03-25 14:36:55 -0400

    update log

--- mygrad/operations/log.py
+++ mygrad/operations/log.py
@@ -1,4 +1,4 @@
-from .operation_base import Operation
+from mygrad.operations.multivar_operations import MultiVarOperation
 import numpy as np
 
 __all__ = ["Log",
@@ -6,34 +6,37 @@ __all__ = ["Log",
            "Log10"]
 
 
-class Log(Operation):
+class Log(MultiVarOperation):
     def __call__(self, a):
-        self.a = a
+        self.variables = (a,)
         if np.any(a <= 0):
             raise ValueError("Invalid log-domain value")
         return np.log(a.data)
 
-    def backward_a(self, grad):
-        return self.a.backward(grad / self.a.data)
+    def backward_var(self, grad, index, **kwargs):
+        a = self.variables[index]
+        a.backward(grad / a.data, **kwargs)
 
 
-class Log2(Operation):
+class Log2(MultiVarOperation):
     def __call__(self, a):
-        self.a = a
+        self.variables = (a,)
         if np.any(a <= 0):
             raise ValueError("Invalid log-domain value")
         return np.log2(a.data)
 
-    def backward_a(self, grad):
-        return self.a.backward(grad / (self.a.data * np.log(2)))
+    def backward_var(self, grad, index, **kwargs):
+        a = self.variables[index]
+        a.backward(grad / (a.data * np.log(2)), **kwargs)
 
 
-class Log10(Operation):
+class Log10(MultiVarOperation):
     def __call__(self, a):
-        self.a = a
+        self.variables = (a,)
         if np.any(a <= 0):
             raise ValueError("Invalid log-domain value")
         return np.log10(a.data)
 
-    def backward_a(self, grad):
-        return self.a.backward(grad / (self.a.data * np.log(10)))
+    def backward_var(self, grad, index, **kwargs):
+        a = self.variables[index]
+        a.backward(grad / (a.data * np.log(10)), **kwargs)
