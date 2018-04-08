fix logaddexp2 backprop

mygrad/math/exp_log/ops.py
@@ -67,12 +67,10 @@ class Logaddexp2(BroadcastableOp):
     def backward_var(self, grad, index, **kwargs):
         a, b = self.variables
         if index == 0:
-            dLda = grad / (1 + np.exp(b.data - a.data))
-            dLda /= np.log(2)
+            dLda = grad / (1 + 2 ** (b.data - a.data))
             a.backward(dLda, **kwargs)
         else:
-            dLdb = grad / (1 + np.exp(a.data - b.data))
-            dLdb /= np.log(2)
+            dLdb = grad / (1 + 2 ** (a.data - b.data))
             b.backward(dLdb, **kwargs)
 
 

mygrad/tensor_core_ops/indexing.py
@@ -139,7 +139,8 @@ class SetItem(BroadcastableOp):
                     if len(first_inds) < len(sub_sel):
                         # one or more elements were set redundantly, identify the entries in `b`
                         # that actually were set to those elements (the last-most set-item calls
-                        # for those elements).
+                        # for those elements) and propagate only the corresponding elements from grad
+
                         first_inds = (len(sub_sel) - 1) - first_inds
                         mask = np.zeros_like(sub_sel)
                         mask[first_inds] = 1