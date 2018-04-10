bug fix: maximum/minimum gave bad gradient where x == y; unit test added for that case

mygrad/math/misc/ops.py
@@ -50,36 +50,36 @@ class Cbrt(Operation):
 class Maximum(BroadcastableOp):
     def __call__(self, a, b):
         self.variables = (a, b)
-        self.max_mask = a.data > b.data
+        self.greater_than_mask = a.data > b.data
         self.equal_mask = a.data == b.data
-        return np.where(self.max_mask, a.data, b.data)
+        return np.where(self.greater_than_mask, a.data, b.data)
 
     def backward_var(self, grad, index, **kwargs):
         if index == 0:
-            mask = self.max_mask
+            mask = self.greater_than_mask
         elif index == 1:
-            mask = np.logical_not(self.max_mask)
+            mask = np.logical_not(self.greater_than_mask)
+            np.logical_not(mask, out=mask, where=self.equal_mask)
         else:
             raise IndexError
 
-        #np.logical_not(mask, out=mask, where=self.equal_mask)
         self.variables[index].backward(mask * grad, **kwargs)
 
 
 class Minimum(BroadcastableOp):
     def __call__(self, a, b):
         self.variables = (a, b)
-        self.max_mask = a.data < b.data
+        self.less_than_mask = a.data < b.data
         self.equal_mask = a.data == b.data
-        return np.where(self.max_mask, a.data, b.data)
+        return np.where(self.less_than_mask, a.data, b.data)
 
     def backward_var(self, grad, index, **kwargs):
         if index == 0:
-            mask = self.max_mask
+            mask = self.less_than_mask
         elif index == 1:
-            mask = np.logical_not(self.max_mask)
+            mask = np.logical_not(self.less_than_mask)
+            np.logical_not(mask, out=mask, where=self.equal_mask)
         else:
             raise IndexError
 
-        #np.logical_not(mask, out=mask, where=self.equal_mask)
         self.variables[index].backward(mask * grad, **kwargs)

tests/math/binary/test_binary_funcs.py
@@ -92,8 +92,6 @@ def test_maximum_fwd(): pass
                     elements=st.floats(-10., 10.)),
        data=st.data())
 def test_maximum_bkwd(x, data):
-    """ index conforms strictly to basic indexing """
-
     y = data.draw(hnp.arrays(shape=broadcastable_shape(x.shape, max_dim=5),
                              dtype=float,
                              elements=st.floats(-10., 10.)), label="y")
@@ -117,6 +115,31 @@ def test_maximum_bkwd(x, data):
 
     assert_allclose(x_arr.grad, dx)
     assert_allclose(y_arr.grad, dy)
+
+
+def test_maximum_minimum_bkwd_equal():
+    """ regression test for documented behavior of maximum/minimum where
+        x == y"""
+
+    x = Tensor([1., 0., 2.])
+    y = Tensor([2., 0., 1.])
+
+    o = maximum(x, y)
+    o.backward()
+
+    assert_allclose(x.grad, [0., 0., 1])
+    assert_allclose(y.grad, [1., 0., 0])
+    o.null_gradients()
+
+    x = Tensor([1., 0., 2.])
+    y = Tensor([2., 0., 1.])
+
+    o = minimum(x, y)
+    o.backward()
+
+    assert_allclose(x.grad, [1., 0., 0.])
+    assert_allclose(y.grad, [0., 0., 1.])
+    o.null_gradients()
 
 
 @fwdprop_test_factory(mygrad_func=minimum, true_func=np.minimum, num_arrays=2)