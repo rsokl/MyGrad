f8f67d90ab7f849ecb8ddaeec3c414872e0004f8
Ryan Soklaski <ry26099@mit.edu>
2018-06-06 17:04:48 -0400

    add arithmetic func docs

--- mygrad/math/arithmetic/funcs.py
+++ mygrad/math/arithmetic/funcs.py
@@ -16,30 +16,131 @@ __all__ = ["add",
 
 
 def add(a, b, constant=False):
+    """ f(a, b) -> a + b
+
+        Parameters
+        ----------
+        a : array_like
+
+        b : array_like
+
+        constant : bool, optional(default=True)
+            If `True`, the returned tensor is a constant (it
+            does not backpropagate a gradient)
+
+        Returns
+        -------
+        mygrad.Tensor"""
     return Tensor._op(Add, a, b, constant=constant)
 
 
 def subtract(a, b, constant=False):
+    """ f(a, b) -> a - b
+
+        Parameters
+        ----------
+        a : array_like
+
+        b : array_like
+
+        constant : bool, optional(default=True)
+            If `True`, the returned tensor is a constant (it
+            does not backpropagate a gradient)
+
+        Returns
+        -------
+        mygrad.Tensor"""
     return Tensor._op(Subtract, a, b, constant=constant)
 
 
 def divide(a, b, constant=False):
+    """ f(a, b) -> a / b
+
+        Parameters
+        ----------
+        a : array_like
+
+        b : array_like
+
+        constant : bool, optional(default=True)
+            If `True`, the returned tensor is a constant (it
+            does not backpropagate a gradient)
+
+        Returns
+        -------
+        mygrad.Tensor"""
     return Tensor._op(Divide, a, b, constant=constant)
 
 
 def reciprocal(a, constant=False):
+    """ f(a) -> 1 / a
+
+        Parameters
+        ----------
+        a : array_like
+
+        constant : bool, optional(default=True)
+            If `True`, the returned tensor is a constant (it
+            does not backpropagate a gradient)
+
+        Returns
+        -------
+        mygrad.Tensor"""
     return Tensor._op(Reciprocal, a, constant=constant)
 
 
 def power(a, b, constant=False):
+    """ f(a, b) -> a ** b
+
+        Parameters
+        ----------
+        a : array_like
+
+        b : array_like
+
+        constant : bool, optional(default=True)
+            If `True`, the returned tensor is a constant (it
+            does not backpropagate a gradient)
+
+        Returns
+        -------
+        mygrad.Tensor"""
     return Tensor._op(Power, a, b, constant=constant)
 
 
 def multiply(a, b, constant=False):
+    """ f(a, b) -> a * b
+
+        Parameters
+        ----------
+        a : array_like
+
+        b : array_like
+
+        constant : bool, optional(default=True)
+            If `True`, the returned tensor is a constant (it
+            does not backpropagate a gradient)
+
+        Returns
+        -------
+        mygrad.Tensor"""
     return Tensor._op(Multiply, a, b, constant=constant)
 
 
 def multiply_sequence(*variables, constant=False):
+    """ f(a, b) -> a + b
+
+        Parameters
+        ----------
+        variables : array_like
+
+        constant : bool, optional(default=True)
+            If `True`, the returned tensor is a constant (it
+            does not backpropagate a gradient)
+
+        Returns
+        -------
+        mygrad.Tensor"""
     return Tensor._op(MultiplySequence, *variables, constant=constant)
 
 
@@ -48,8 +149,46 @@ def add_sequence(*variables, constant=False):
 
 
 def positive(a, where=True, constant=False):
+    """ f(a) -> +a
+
+        Parameters
+        ----------
+        a : array_like
+
+        where : numpy.ndarray
+            Accepts a boolean array which is broadcast together
+            with the operand(s). Values of True indicate to calculate
+            the function at that position, values of False indicate
+            to leave the value in the output alone.
+
+        constant : bool, optional(default=True)
+            If `True`, the returned tensor is a constant (it
+            does not backpropagate a gradient)
+
+        Returns
+        -------
+        mygrad.Tensor"""
     return Tensor._op(Positive, a, op_kwargs=(dict(where=where)), constant=constant)
 
 
 def negative(a, where=True, constant=False):
+    """ f(a) -> -a
+
+        Parameters
+        ----------
+        a : array_like
+
+        where : numpy.ndarray
+            Accepts a boolean array which is broadcast together
+            with the operand(s). Values of True indicate to calculate
+            the function at that position, values of False indicate
+            to leave the value in the output alone.
+
+        constant : bool, optional(default=True)
+            If `True`, the returned tensor is a constant (it
+            does not backpropagate a gradient)
+
+        Returns
+        -------
+        mygrad.Tensor"""
     return Tensor._op(Negative, a, op_kwargs=(dict(where=where)), constant=constant)
