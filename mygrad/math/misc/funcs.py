eab4b4fd59ab85d2bc71b4760d082365616eb725
Ryan Soklaski <ry26099@mit.edu>
2018-06-06 17:29:01 -0400

    math/misc docs

--- mygrad/math/misc/funcs.py
+++ mygrad/math/misc/funcs.py
@@ -10,23 +10,79 @@ __all__ = ["abs",
 
 
 def abs(a, constant=False):
-    """ f(a)-> |a|
+    """ ``f(a) -> abs(a)``
 
+        Parameters
+        ----------
+        a : array_like
+
+        constant : bool, optional(default=False)
+            If ``True``, the returned tensor is a constant (it
+            does not backpropagate a gradient)
+
+        Returns
+        -------
+        mygrad.Tensor
+
+        Notes
+        -----
         The derivative at a == 0 returns nan"""
     return Tensor._op(Abs, a, constant=constant)
 
 
 def sqrt(a, constant=False):
+    """ ``f(a) -> sqrt(a)``
+
+        Parameters
+        ----------
+        a : array_like
+
+        constant : bool, optional(default=False)
+            If ``True``, the returned tensor is a constant (it
+            does not backpropagate a gradient)
+
+        Returns
+        -------
+        mygrad.Tensor"""
     return Tensor._op(Sqrt, a, constant=constant)
 
 
 def cbrt(a, constant=False):
+    """ ``f(a) -> cbrt(a)``
+
+        Parameters
+        ----------
+        a : array_like
+
+        constant : bool, optional(default=False)
+            If ``True``, the returned tensor is a constant (it
+            does not backpropagate a gradient)
+
+        Returns
+        -------
+        mygrad.Tensor"""
     return Tensor._op(Cbrt, a, constant=constant)
 
 
 def maximum(a, b, constant=False):
     """ Element-wise maximum of array elements.
 
+        Parameters
+        ----------
+        a : array_like
+
+        b : array_like
+
+        constant : bool, optional(default=False)
+            If ``True``, the returned tensor is a constant (it
+            does not backpropagate a gradient)
+
+        Returns
+        -------
+        mygrad.Tensor
+
+        Notes
+        -----
         The gradient does not exist where a == b; we use a
         value of 0 here."""
     return Tensor._op(Maximum, a, b, constant=constant)
@@ -35,6 +91,22 @@ def maximum(a, b, constant=False):
 def minimum(a, b, constant=False):
     """ Element-wise minimum of array elements.
 
+        Parameters
+        ----------
+        a : array_like
+
+        b : array_like
+
+        constant : bool, optional(default=False)
+            If ``True``, the returned tensor is a constant (it
+            does not backpropagate a gradient)
+
+        Returns
+        -------
+        mygrad.Tensor
+
+        Notes
+        -----
         The gradient does not exist where a == b; we use a
         value of 0 here."""
     return Tensor._op(Minimum, a, b, constant=constant)
