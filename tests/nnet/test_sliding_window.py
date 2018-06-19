3767460a458a5653dd96d94d088360f7cdca111b
Ryan Soklaski <ry26099@mit.edu>
2018-06-19 19:46:44 -0400

    add dtype search for sliding-window unit test

--- tests/nnet/test_sliding_window.py
+++ tests/nnet/test_sliding_window.py
@@ -8,11 +8,16 @@ from hypothesis import given
 import hypothesis.extra.numpy as hnp
 
 
+dtype_strat_numpy = st.sampled_from((np.int8, np.int16, np.int32, np.int64,
+                                     np.float16, np.float32, np.float64))
+
+
 @given(data=st.data(),
-       x=hnp.arrays(dtype=float, shape=hnp.array_shapes(max_dims=5, min_dims=1)))
+       x=hnp.arrays(dtype=dtype_strat_numpy, shape=hnp.array_shapes(max_dims=5, min_dims=1, max_side=20)))
 def test_sliding_window(data, x):
     """ Test variations of window-shape, step, and dilation for sliding window
         view of N-dimensional array."""
+
     win_dim = data.draw(st.integers(1, x.ndim), label="win_dim")
     win_shape = data.draw(st.tuples(*(st.integers(1, s) for s in x.shape[-win_dim:])),
                           label="win_shape")
@@ -20,8 +25,8 @@ def test_sliding_window(data, x):
                      label="step")
 
     max_dilation = np.array(x.shape[-win_dim:]) // win_shape
-    dilation = data.draw(st.one_of(st.none(), st.tuples(*(st.integers(1, s) for s in max_dilation))), label="dilation")
-
+    dilation = data.draw(st.one_of(st.none(), st.tuples(*(st.integers(1, s) for s in max_dilation))),
+                         label="dilation")
     y = sliding_window_view(x, window_shape=win_shape, step=step, dilation=dilation)
 
     if dilation is None:
@@ -32,12 +37,13 @@ def test_sliding_window(data, x):
         assert_allclose(actual=y[tuple([*ind])], desired=x[(..., *slices)])
 
 
-def test_memory_details():
+@given(dtype=dtype_strat_numpy)
+def test_memory_details(dtype):
     """ Ensure that:
           - function handles non C-contiguous layouts correctly
           - output is view of input
           - output is not writeable"""
-    x = np.arange(20).reshape(2, 10)
+    x = np.arange(20).reshape(2, 10).astype(dtype)
     x = np.asfortranarray(x)
     y = sliding_window_view(x, (5,), 5)
     soln = np.array([[[ 0,  1,  2,  3,  4],
