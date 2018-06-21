1f3769d1e180fd42c682506b0a6b89fca129f0b8
Anonymission <marrs2k@gmail.com>
2018-06-21 14:38:33 -0400

    add labels to nodes

--- mygrad/computational_graph.py
+++ mygrad/computational_graph.py
@@ -20,10 +20,19 @@ def build_graph(fin):
 
     for out, op in fin._graph_dict.items():
         if op is not None:
-            op_node = pydot.Node(op.__repr__().rpartition(".")[-1].replace(" object at ", "\n")[:-1], style="filled", fillcolor="red")
+            op_name = op.__repr__().rpartition(".")[-1].replace(" object at ", "\n")[:-1]
+            op_node = pydot.Node(name=op_name, label=op_name.rpartition("\n")[0], style="filled", fillcolor="red")
             graph.add_node(op_node)
-            for var in op.variables:
-                graph.add_edge(pydot.Edge(str(var.data) + "\n" + str(id(var)), op_node))
-            graph.add_edge(pydot.Edge(op_node, out))
 
-    graph.write_png('network_graph.png')
+            for var in op.variables:
+                var_name = str(var.data) + "\n" + str(id(var))
+                var_node = pydot.Node(name=var_name, label=var_name.rpartition("\n")[0])
+                graph.add_node(var_node)
+                graph.add_edge(pydot.Edge(var_node, op_node))
+
+            out_label = out.rpartition("\n")[0]
+            out_node = pydot.Node(name=out, label=out_label)
+            graph.add_node(out_node)
+            graph.add_edge(pydot.Edge(op_node, out_node))
+
+    graph.write_png('computational_graph.png')
