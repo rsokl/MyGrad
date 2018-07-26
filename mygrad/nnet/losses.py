f70a43946d1b14d5b9bb216a4e9ebea0c26555a3
Ryan Soklaski <ry26099@mit.edu>
2018-07-26 14:08:27 -0400

    add margin ranking loss and unit test

--- mygrad/nnet/losses.py
+++ mygrad/nnet/losses.py
@@ -2,8 +2,9 @@ from mygrad.operation_base import Operation
 from mygrad.tensor_base import Tensor
 import numpy as np
 from scipy.misc import logsumexp
+from numbers import Real
 
-__all__ = ["multiclass_hinge"]
+__all__ = ["multiclass_hinge", "softmax_crossentropy", "margin_ranking_loss"]
 
 
 class MulticlassHinge(Operation):
@@ -123,3 +124,84 @@ def softmax_crossentropy(x, y_true, constant=False):
     return Tensor._op(SoftmaxCrossEntropy, x, op_args=(y_true,), constant=constant)
 
 
+class MarginRanking(Operation):
+    def __call__(self, x1, x2, y, margin):
+        """
+
+        Parameters
+        ----------
+        x1 : mygrad.Tensor, shape=(N,) or (N, D)
+        x2 : mygrad.Tensor, shape=(N,) or (N, D)
+        y : numpy.ndarray
+        margin : float
+
+        Returns
+        -------
+        numpy.ndarray, shape=()
+        """
+        self.variables = (x1, x2)
+        x1 = x1.data
+        x2 = x2.data
+
+        self.y = y
+
+        M = margin - self.y * (x1 - x2)
+        not_thresh = M <= 0
+        loss = M
+        loss[not_thresh] = 0.
+
+        self._grad = np.ones_like(M)
+        self._grad[not_thresh] = 0.
+        self._grad /= M.size
+        return np.mean(loss)
+
+    def backward_var(self, grad, index, **kwargs):
+        sign = -self.y if index == 0 else self.y
+        self.variables[index].backward(grad * (sign * self._grad), **kwargs)
+
+
+def margin_ranking_loss(x1, x2, y, margin, constant=False):
+    """
+    Computes the margin average margin ranking loss.
+
+    Equivalent to:
+             mg.mean(mg.maximum(0, margin - y * (x1 - x2)))
+
+    Parameters
+    ----------
+    x1 : array_like, shape=(N,) or (N, D)
+        A batch of scores or descriptors to compare against those in `x2`
+
+    x2 : array_like, shape=(N,) or (N, D)
+        A batch of scores or descriptors to compare against those in `x1`
+
+    y  : Union[int, array_like], scalar or shape=(N,)
+        1 or -1. Specifies whether the margin is compared against `(x1 - x2)`
+        or `(x2 - x1)`, for each of the N comparisons.
+
+    margin : float
+        A non-negative value to be used as the margin for the loss.
+
+    constant : bool, optional(default=False)
+        If ``True``, the returned tensor is a constant (it
+        does not back-propagate a gradient)
+
+    Returns
+    -------
+    mygrad.Tensor, shape=()
+        The mean margin ranking loss.
+    """
+    assert 0 < x1.ndim < 3, "`x1` must have shape (N,) or (N, D)"
+    assert x1.shape == x2.shape, "`x1` and `x2` must have the same shape"
+    assert np.issubdtype(x1.dtype, np.floating), "`x1` must contain floats"
+    assert isinstance(margin, Real) and margin >= 0, "`margin` must be a non-negative scalar"
+    if isinstance(y, Tensor):
+        y = y.data
+
+    y = np.asarray(y)
+    assert y.ndim == 0 or (y.ndim == 1 and len(y) == len(x1)), "`y` must be a scalar or shape-(N,) array of ones"
+    if y.ndim:
+        assert y.size == 1 or len(y) == len(x1)
+        if x1.ndim == 2:
+            y = y.reshape(-1, 1)
+    return Tensor._op(MarginRanking, x1, x2, op_args=(y, margin), constant=constant)
---
+++ tests/nnet/test_margin_ranking.py
@@ -0,0 +1,77 @@
+import numpy as np
+import mygrad as mg
+from mygrad.nnet.losses import margin_ranking_loss
+
+from numpy.testing import assert_allclose, assert_array_equal
+import hypothesis.strategies as st
+from hypothesis import given
+import hypothesis.extra.numpy as hnp
+
+
+def simple_loss(x1, x2, y, margin):
+    """
+    x1 : mygrad.Tensor, shape=(N, D)
+    x2 : mygrad.Tensor, shape=(N, D)
+    y : Union[int, numpy.ndarray], scalar or shape=(N,)
+    margin : float
+
+    Returns
+    -------
+    mygrad.Tensor, shape=()
+    """
+    y = np.asarray(y)
+    if y.ndim:
+        assert y.size == 1 or len(y) == len(x1)
+        if x1.ndim == 2:
+            y = y.reshape(-1, 1)
+
+    return mg.mean(mg.maximum(0, margin - y * (x1 - x2)))
+
+
+@given(shape=hnp.array_shapes(min_dims=1, max_dims=2),
+       margin=st.floats(0, 1000),
+       data=st.data())
+def test_ranked_margin(shape, margin, data):
+    x1 = data.draw(hnp.arrays(shape=shape, dtype=float, elements=st.floats(-1000, 1000)), label="x1")
+    x2 = data.draw(hnp.arrays(shape=shape, dtype=float, elements=st.floats(-1000, 1000)), label="x2")
+    y = data.draw(st.one_of(st.sampled_from((-1, 1)),
+                            hnp.arrays(shape=shape[:1],
+                                       dtype=int,
+                                       elements=st.sampled_from((-1, 1)))),
+                  label="y")
+
+    x1_copy = np.copy(x1)
+    x2_copy = np.copy(x2)
+    y_copy = np.copy(y)
+
+    x1_dum = mg.Tensor(x1)
+    x2_dum = mg.Tensor(x2)
+
+    x1_real = mg.Tensor(x1)
+    x2_real = mg.Tensor(x2)
+
+    loss_dum = simple_loss(x1_dum, x2_dum, y, margin)
+
+    loss_real = margin_ranking_loss(x1_real, x2_real, y, margin)
+
+    assert_allclose(actual=loss_real.data, desired=loss_dum.data, err_msg="losses don't match")
+
+    assert_array_equal(x1, x1_copy, err_msg="`x1` was mutated by forward")
+    assert_array_equal(x2, x2_copy, err_msg="`x2` was mutated by forward")
+    if isinstance(y, np.ndarray):
+        assert_array_equal(y, y_copy, err_msg="`y` was mutated by forward")
+
+    loss_dum.backward()
+    loss_real.backward()
+
+    assert_allclose(actual=x1_real.grad, desired=x1_dum.grad, err_msg="x1.grad doesn't match")
+    assert_allclose(actual=x2_real.grad, desired=x2_dum.grad, err_msg="x2.grad doesn't match")
+
+    assert_array_equal(x1, x1_copy, err_msg="`x1` was mutated by backward")
+    assert_array_equal(x2, x2_copy, err_msg="`x2` was mutated by backward")
+    if isinstance(y, np.ndarray):
+        assert_array_equal(y, y_copy, err_msg="`y` was mutated by backward")
+
+    loss_real.null_gradients()
+    assert x1_real.grad is None
+    assert x2_real.grad is None
