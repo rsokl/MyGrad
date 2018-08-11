57681f396d21ebb29803c29d165b2d763db52969
Ryan Soklaski <ry26099@mit.edu>
2018-08-11 19:09:14 -0400

    remove deprecated dense layer

--- mygrad/nnet/layers/__init__.py
+++ mygrad/nnet/layers/__init__.py
@@ -1,8 +1,7 @@
-from .dense import dense
 from .conv import conv_nd
 from .pooling import max_pool
 from .recurrent import simple_RNN
 from .gru import gru
 
-__all__ = ["dense", "conv_nd", "max_pool", "simple_RNN", "gru"]
+__all__ = ["conv_nd", "max_pool", "simple_RNN", "gru"]
 
