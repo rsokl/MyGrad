176c40c8921b63ed60a179d26840c61d59825d2a
student <student@2B14-iMac.local>
2018-09-05 11:14:25 -0400

    Use hypothesis to generate test data

--- tests/math/unary/test_nondifferentiable.py
+++ tests/math/unary/test_nondifferentiable.py
@@ -2,24 +2,30 @@ from mygrad import argmin, argmax
 from mygrad.tensor_base import Tensor
 
 import hypothesis.strategies as st
-from numpy.testing import assert_equal
+from numpy.testing import assert_allclose
 import numpy as np
+
+from ...custom_strategies import valid_axes
 from hypothesis import given
+import hypothesis.extra.numpy as hnp
 
 import numpy as np
 
-@given(d=st.integers(min_value=3, max_value=10))
-def test_argmin(d):
-    dimensions = tuple(np.random.randint(1,6) for i in range(d)) 
+dtype_strat_numpy = st.sampled_from((np.int8, np.int16, np.int32, np.int64,
+                                     np.float16, np.float32, np.float64))
 
-    a = np.random.randint(-100,100, size=dimensions)
-    axis = np.random.randint(0,d-1)
-    assert_equal(Tensor(a).argmin(axis=axis), a.argmin(axis=axis))
+@given(a=hnp.arrays(shape=hnp.array_shapes(max_side=4, max_dims=5),
+                    dtype=dtype_strat_numpy),
+       data=st.data())
+def test_argmin(a, data):
+    axis = data.draw(valid_axes(ndim=a.ndim, single_axis_only=True), label="axis")
 
-@given(d=st.integers(min_value=3, max_value=10))
-def test_argmax(d):
-    dimensions = tuple(np.random.randint(1,6) for i in range(d)) 
+    assert_allclose(Tensor(a).argmin(axis=axis), a.argmin(axis=axis))
 
-    a = np.random.randint(-100,100, size=dimensions)
-    axis = np.random.randint(0,d-1)
-    assert_equal(Tensor(a).argmax(axis=axis), a.argmax(axis=axis))
+@given(a=hnp.arrays(shape=hnp.array_shapes(max_side=4, max_dims=5),
+                    dtype=dtype_strat_numpy),
+       data=st.data())
+def test_argmax(a, data):
+    axis = data.draw(valid_axes(ndim=a.ndim, single_axis_only=True), label="axis")
+    
+    assert_allclose(Tensor(a).argmax(axis=axis), a.argmax(axis=axis))
