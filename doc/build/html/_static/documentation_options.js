0bff9205648ee84393e4ab32f1b5863231504733
Ryan Soklaski <ry26099@mit.edu>
2018-11-18 16:02:18 -0500

    fix version in docs

--- doc/build/html/_static/documentation_options.js
+++ doc/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+unknown',
+    VERSION: '0.9.0+0.g66b220c.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
