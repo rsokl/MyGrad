ab86fd88edd97399213c6903cd4d6cac9f2ee3e1
Ryan Soklaski <ry26099@mit.edu>
2018-12-30 15:52:02 -0500

    converting seq-tests to use uber:: mean-bkwd failing!

--- tests/math/sequence/test_sequential_funcs.py
+++ tests/math/sequence/test_sequential_funcs.py
@@ -1,5 +1,8 @@
 from ...utils.numerical_gradient import numerical_gradient_full
 from ...wrappers.sequence_func import fwdprop_test_factory, backprop_test_factory
+from ...wrappers.uber import fwdprop_test_factory as uber_fwd
+from ...wrappers.uber import backprop_test_factory as uber_bkwd
+from ...custom_strategies import valid_axes
 from numpy.testing import assert_allclose
 from pytest import raises
 
@@ -12,20 +15,42 @@ from mygrad import Tensor
 from hypothesis import given, settings
 import hypothesis.strategies as st
 import hypothesis.extra.numpy as hnp
+from hypothesis import note
+
+def axis_arg(*arrs): return valid_axes(arrs[0].ndim)
+
+def keepdims_arg(*arrs): return st.booleans()
+
+def ddof_arg(*arrs): return st.integers(0, min(arrs[0].shape) - 1)
+
 
 @fwdprop_test_factory(mygrad_func=amax, true_func=np.amax)
 def test_max_fwd(): pass
 
 
+@uber_fwd(mygrad_func=amax, true_func=np.amax, num_arrays=1,
+          kwargs=dict(axis=axis_arg, keepdims=keepdims_arg))
+def test_max_fwd_uber(): pass
+
+@uber_bkwd(mygrad_func=amax, true_func=np.amax, num_arrays=1,
+           kwargs=dict(axis=axis_arg, keepdims=keepdims_arg),
+           vary_each_element=True, index_to_unique={0: True},
+           elements_strategy=st.integers)
+def test_max_bkwd_uber(): pass
+
 @backprop_test_factory(mygrad_func=amax, true_func=np.amax, unique=True)
 def test_max_bkwd(): pass
 
 
-@fwdprop_test_factory(mygrad_func=amin, true_func=np.amin)
+@uber_fwd(mygrad_func=amin, true_func=np.amin, num_arrays=1,
+          kwargs=dict(axis=axis_arg, keepdims=keepdims_arg))
 def test_min_fwd(): pass
 
 
-@backprop_test_factory(mygrad_func=amin, true_func=np.amin, unique=True)
+@uber_bkwd(mygrad_func=amin, true_func=np.amin, num_arrays=1,
+           kwargs=dict(axis=axis_arg, keepdims=keepdims_arg),
+           vary_each_element=True, index_to_unique={0: True},
+           elements_strategy=st.integers)
 def test_min_bkwd(): pass
 
 
@@ -34,62 +59,66 @@ def test_min_max_aliases():
     assert mygrad.min == amin
 
 
-@fwdprop_test_factory(mygrad_func=sum, true_func=np.sum)
+@uber_fwd(mygrad_func=sum, true_func=np.sum, num_arrays=1,
+          kwargs=dict(axis=axis_arg, keepdims=keepdims_arg))
 def test_sum_fwd(): pass
 
 
-@backprop_test_factory(mygrad_func=sum, true_func=np.sum)
+@uber_bkwd(mygrad_func=sum, true_func=np.sum, num_arrays=1,
+           kwargs=dict(axis=axis_arg, keepdims=keepdims_arg),
+           vary_each_element=True)
 def test_sum_bkwd(): pass
 
 
-@fwdprop_test_factory(mygrad_func=mean, true_func=np.mean)
+@uber_fwd(mygrad_func=mean, true_func=np.mean, num_arrays=1,
+          kwargs=dict(axis=axis_arg, keepdims=keepdims_arg))
 def test_mean_fwd(): pass
 
 
-@backprop_test_factory(mygrad_func=mean, true_func=np.mean)
+@uber_bkwd(mygrad_func=mean, true_func=np.mean, num_arrays=1,
+           kwargs=dict(axis=axis_arg, keepdims=keepdims_arg),
+           vary_each_element=True)
 def test_mean_bkwd(): pass
 
 
-@fwdprop_test_factory(mygrad_func=var, true_func=np.var)
+@uber_fwd(mygrad_func=var, true_func=np.var, num_arrays=1,
+          kwargs=dict(axis=axis_arg, keepdims=keepdims_arg,
+                      ddof=ddof_arg))
 def test_var_fwd(): pass
 
 
-@backprop_test_factory(mygrad_func=var, true_func=np.var)
+@uber_bkwd(mygrad_func=var, true_func=np.var, num_arrays=1,
+           kwargs=dict(axis=axis_arg, keepdims=keepdims_arg,
+                       ddof=ddof_arg),
+           vary_each_element=True, index_to_bnds={0: (-10, 10)},
+           atol=1e-5, rtol=1e-5)
 def test_var_bkwd(): pass
 
 
-@given(x=hnp.arrays(shape=(10,), dtype=float, elements=st.floats(-10, 10)),
-       ddof=st.integers(0, 9))
-def test_var_ddof(x, ddof):
-    assert np.var(x, ddof=ddof) == var(x, ddof=ddof).data
-
-
-@given(x=hnp.arrays(shape=(10,), dtype=float, elements=st.floats(-10, 10)),
-       ddof=st.integers(0, 9))
-def test_var_ddof_backward(x, ddof):
-    y = Tensor(x)
-
-    def f(z): return np.var(z, ddof=ddof)
-
-    o = var(y, ddof=ddof)
-    o.backward(2.)
-
-    g, = numerical_gradient_full(f, x, back_grad=np.asarray(2.))
-    assert_allclose(g, y.grad, rtol=1e-5, atol=1e-5)
-
-
 # std composes mygrad's sqrt and var, backprop need not be tested
-@fwdprop_test_factory(mygrad_func=std, true_func=np.std, unique=True)
+@uber_fwd(mygrad_func=std, true_func=np.std, num_arrays=1,
+          kwargs=dict(axis=axis_arg, keepdims=keepdims_arg,
+                      ddof=ddof_arg))
 def test_std_fwd(): pass
 
 
-@given(x=hnp.arrays(shape=(10,), dtype=float, elements=st.floats(-10, 10)),
-       ddof=st.integers(0, 9))
-def test_std_ddof(x, ddof):
-    assert np.std(x, ddof=ddof) == std(x, ddof=ddof).data
+def _assume(*arrs, **kwargs):
+    return all(i > 1 for i in arrs[0].shape)
 
 
-@fwdprop_test_factory(mygrad_func=prod, true_func=np.prod)
+@uber_bkwd(mygrad_func=std, true_func=np.std, num_arrays=1,
+           kwargs=dict(axis=axis_arg, keepdims=keepdims_arg,
+                       ddof=ddof_arg),
+           vary_each_element=True, index_to_bnds={0: (-10, 10)},
+           elements_strategy=st.integers,
+           index_to_unique={0: True},
+           assumptions=_assume,
+           atol=1e-5, rtol=1e-5)
+def test_std_bkwd(): pass
+
+
+@uber_fwd(mygrad_func=prod, true_func=np.prod, num_arrays=1,
+          kwargs=dict(axis=axis_arg, keepdims=keepdims_arg))
 def test_prod_fwd(): pass
 
 
