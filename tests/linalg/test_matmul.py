fix domains for numerical stability

tests/linalg/test_matmul.py
@@ -43,7 +43,8 @@ def test_matmul_fwd_static():
 @backprop_test_factory(mygrad_func=matmul, true_func=np.matmul, num_arrays=2,
                        index_to_arr_shapes={0: (4,), 1:(4,)}, as_decimal=False,
                        vary_each_element=True,
-                       atol=1e-5, rtol=1e-5)
+                       atol=1e-5, rtol=1e-5,
+                       index_to_bnds={0: (-10, 10), 1: (-10, 10)})
 def test_matmul_bkwd_1d_1d():
     """ a is 1-d, b is 1-d"""
     pass
@@ -54,7 +55,8 @@ def test_matmul_bkwd_1d_1d():
                                             1: (4,)}, 
                        as_decimal=False,
                        vary_each_element=True,
-                       atol=1e-4, rtol=1e-4)
+                       atol=1e-4, rtol=1e-4,
+                       index_to_bnds={0: (-10, 10), 1: (-10, 10)})
 def test_matmul_bkwd_nd_1d():
     """ a is n-d, b is 1-d"""
     pass
@@ -65,7 +67,8 @@ def test_matmul_bkwd_nd_1d():
                                             0: (4,)}, 
                        as_decimal=False,
                        vary_each_element=True,
-                       atol=1e-5, rtol=1e-5)
+                       atol=1e-5, rtol=1e-5,
+                       index_to_bnds={0: (-10, 10), 1: (-10, 10)})
 def test_matmul_bkwd_1d_nd():
     """ a is 1-d, b is n-d"""
     pass
@@ -77,7 +80,8 @@ def test_matmul_bkwd_1d_nd():
                                             1: (4, 5)}, 
                        as_decimal=False,
                        vary_each_element=True,
-                       atol=1e-4, rtol=1e-4)
+                       atol=1e-4, rtol=1e-4,
+                       index_to_bnds={0: (-10, 10), 1: (-10, 10)})
 def test_matmul_bkwd_nd_nd():
     """ a is n-d, b is n-d; b can broadcast into a"""
     pass
@@ -89,7 +93,8 @@ def test_matmul_bkwd_nd_nd():
                                             1: special_shape((4, 5), max_dim=2)}, 
                        as_decimal=False,
                        vary_each_element=True,
-                       atol=1e-3, rtol=1e-3)
+                       atol=1e-3, rtol=1e-3,
+                       index_to_bnds={0: (-10, 10), 1: (-10, 10)})
 def test_matmul_bkwd_nd_nd2():
     """ a is n-d, b is n-d; a can broadcast into b"""
     pass
@@ -101,7 +106,8 @@ def test_matmul_bkwd_nd_nd2():
                                             1: (1, 2, 4, 2)}, 
                        as_decimal=False,
                        vary_each_element=True,
-                       atol=1e-4, rtol=1e-4)
+                       atol=1e-4, rtol=1e-4,
+                       index_to_bnds={0: (-10, 10), 1: (-10, 10)})
 def test_matmul_bkwd_nd_nd3():
     """ a is n-d, b is n-d; a and b broadcast mutually via singleton dimensions"""
     pass

tests/math/unary/test_trigonometric.py
@@ -25,7 +25,8 @@ def test_sinc_backward(): pass
 def test_cos_fwd(): pass
 
 
-@backprop_test_factory(num_arrays=1, mygrad_func=cos, true_func=np.cos, as_decimal=False)
+@backprop_test_factory(num_arrays=1, mygrad_func=cos, true_func=np.cos, as_decimal=False,
+                       atol=1e-5)
 def test_cos_backward(): pass
 
 