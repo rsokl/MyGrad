fix tests with as_decimal=True to retain full precision; add test

tests/utils/numerical_gradient.py
@@ -9,15 +9,17 @@ from mygrad._utils import reduce_broadcast
 def to_decimal_array(arr):
     """ Convert numpy ND-array to Decimal-type object array of the same shape.
         Used for facilitating high-precision arithmetic.
+
         Parameters
         ----------
         arr : Union[float, numpy.ndarray]
+
         Returns
         -------
         numpy.ndarray
-        Decimal-type object array"""
+            Decimal-type object array"""
     arr = np.asarray(arr)
-    print(arr.dtype)
+
     if arr.dtype.kind == "O":
         return arr
     return np.array(tuple(Decimal(float(i)) for i in arr.flat), dtype=Decimal).reshape(arr.shape)
@@ -39,18 +41,23 @@ def numerical_gradient(f, *args, back_grad, vary_ind=None,
             f(x, ...) -> numpy.ndarray
         *args : Tuple[numpy.ndarray, ...]
             The input arguments to be fed to f.
+
         back_grad : numpy.ndarray
             The gradient being back-propagated to x and y, via f
+
         vary_ind : Optional[Tuple[int, ...]]
             If `None`, the partials of f with respect to all the inputs are.
             computed. Otherwise you can specify a sequence of the indices
             of the variables whose partials are to be computed
                0 -> w.r.t x only, 1 -> w.r.t y only, etc.
-        h : float, optional, (default=1e-8)
+
+        h : float, optional, (default=Decimal(1E-8))
             Approximating infinitesimal.
+
         as_decimal : bool, optional (default=True)
             If True, f's arguments are passed as Decimal-type arrays. This
             improves numerical precision, but is not permitted by some functions.
+
         kwargs : Optional[Dict]
 
         Returns
@@ -94,26 +101,31 @@ def numerical_gradient_full(f, *args, back_grad, as_decimal=True, kwargs=None, v
     """ Computes numerical partial derivatives of f(x, y, ..., **kwargs), by
         varying each entry of x, y, ... independently producing a gradient
         in each variable.
+
         Parameters
         ----------
         f : Callable[[numpy.ndarray, ...], numpy.ndarray]
             f(x, ...) -> numpy.ndarray
+
         *args : numpy.ndarray
             The array(s) to be passed to f
+
         back_grad : numpy.ndarray
             The gradient being back-propagated to {x}, via f
-        h : float, optional, (default=1e-8)
-            Approximating infinitesimal.
+
         as_decimal : bool, optional (default=True)
             If True, f's arguments are passed as Decimal-type arrays. This
             improves numerical precision, but is not permitted by some functions.
+
         kwargs : Dict[str, Any], optional (default=dict())
             The keyword arguments to be passed to f.
+
         vary_ind : Optional[Tuple[int, ...]]
             If `None`, the partials of f with respect to all the inputs are.
             computed. Otherwise you can specify a sequence of the indices
             of the variables whose partials are to be computed
                0 -> w.r.t x only, 1 -> w.r.t y only, etc.
+
         Returns
         -------
         Tuple[numpy.ndarray, ...]
@@ -145,20 +157,26 @@ def numerical_gradient_full(f, *args, back_grad, as_decimal=True, kwargs=None, v
 def _numerical_gradient_full(f, *, x, back_grad, h=Decimal(1)/Decimal(int(1e8)), as_decimal=True):
     """ Computes numerical partial derivatives of f(x), by
         varying each entry of `x` independently.
+
         Parameters
         ----------
         f : Callable[[numpy.ndarray], numpy.ndarray]
             f(x) -> numpy.ndarray
+
         x : numpy.ndarray
             An array storing the sequence(s) of values in the array. More than once
             sequence may be designated, according to the `axis` argument of `f`.
+
         back_grad : numpy.ndarray
             The gradient being back-propagated to {x}, via f
-        h : float, optional, (default=1e-8)
+
+        h : float, optional, (default=Decimal(1E-8))
             Approximating infinitesimal.
+
         as_decimal : bool, optional (default=True)
             If True, x is passed to f as a Decimal-type array. This
             improves numerical precision, but is not permitted by some functions.
+
         Returns
         -------
         numpy.ndarray
@@ -167,18 +185,18 @@ def _numerical_gradient_full(f, *, x, back_grad, h=Decimal(1)/Decimal(int(1e8)),
 
     grad = np.empty_like(x)
     x = to_decimal_array(x)
+    x_orig = np.copy(x)
     h = Decimal(h)
     back_grad = to_decimal_array(back_grad)
 
-    x_orig = np.copy(x)
     for ind, val in np.ndenumerate(x):
         x_fwd = x
         x_fwd[ind] = x_orig[ind] + h
         f_fwd = to_decimal_array(f(x_fwd) if as_decimal else f(x_fwd.astype(np.float64)))
 
-        x_bkwd = x_fwd
+        x_bkwd = np.copy(x_fwd)
         x_bkwd[ind] = x_orig[ind] - h
-        f_bkwd = to_decimal_array(f(x_bkwd) if as_decimal else f(x_bkwd.astype(float)))
+        f_bkwd = to_decimal_array(f(x_bkwd) if as_decimal else f(x_bkwd.astype(np.float64)))
 
         df_dxi = to_decimal_array((f_fwd - f_bkwd) / (Decimal(2) * h))
 

tests/utils/test_utils.py
@@ -126,16 +126,14 @@ def test_numerical_gradient_xy_broadcast(data, x, y, grad, as_decimal):
 
 @given(x=hnp.arrays(dtype=float,
                     elements=st.floats(-1, 1),
-                    shape=hnp.array_shapes(max_side=3)),
-       grad=st.floats(-1, 1),
-       keepdims=st.booleans(),
-       as_decimal=st.booleans(),
-       data=st.data())
-def test_numerical_gradient_vary_each(x, grad, keepdims, as_decimal, data):
+                    shape=(2,)),
+       grad=hnp.arrays(dtype=float,
+                       elements=st.floats(-1, 1),
+                       shape=(2,)),
+       as_decimal=st.booleans(),)
+def test_numerical_gradient_vary_each(x, grad, as_decimal):
     atol, rtol = (1e-7, 1e-7) if as_decimal else (1e-2, 1e-2)
-    axes = data.draw(valid_axes(ndim=x.ndim), label="axes")
-    kwargs = dict(axis=axes, keepdims=keepdims)
-    dx, = numerical_gradient_full(np.sum, x, back_grad=np.array(grad), kwargs=kwargs,
+    dx, = numerical_gradient_full(lambda y: y[::-1], x, back_grad=np.array(grad),
                                   as_decimal=as_decimal)
-    x_grad = np.full_like(x, fill_value=grad)
+    x_grad = grad[::-1]
     assert_allclose(actual=dx, desired=x_grad, atol=atol, rtol=rtol)