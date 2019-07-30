d5922474471861cd36ed0bd444a7c7d2b7822ad9
Ryan Soklaski <ry26099@mit.edu>
2019-07-30 16:33:34 -0400

    fix floating point width deprecation in hypothesis

--- tests/tensor_base/test_tensor.py
+++ tests/tensor_base/test_tensor.py
@@ -169,7 +169,11 @@ dtype_strat_numpy = st.sampled_from(
     numpy_dtype=dtype_strat_numpy,
 )
 def test_init_params(data, creator, constant, scalar_only, dtype, numpy_dtype):
-    elements = st.floats if np.issubdtype(numpy_dtype, np.floating) else st.integers
+    elements = (
+        (lambda x, y: st.floats(x, y, width=8*np.dtype(numpy_dtype).itemsize))
+        if np.issubdtype(numpy_dtype, np.floating)
+        else st.integers
+    )
     a = data.draw(
         hnp.arrays(
             shape=hnp.array_shapes(max_side=3, max_dims=5),
@@ -252,7 +256,7 @@ def test_neg(x):
     x=hnp.arrays(
         shape=hnp.array_shapes(),
         dtype=hnp.floating_dtypes(),
-        elements=st.floats(-10, 10),
+        elements=st.floats(-10, 10, width=16),
     ),
     x_constant=st.booleans(),
     y_constant=st.booleans(),
@@ -261,7 +265,7 @@ def test_neg(x):
 def test_comparison_ops(
     op: str, x: np.ndarray, x_constant: bool, y_constant: bool, data: st.SearchStrategy
 ):
-    y = data.draw(hnp.arrays(shape=x.shape, dtype=x.dtype, elements=st.floats(-10, 10)))
+    y = data.draw(hnp.arrays(shape=x.shape, dtype=x.dtype, elements=st.floats(-10, 10, width=16)))
     x = Tensor(x, constant=x_constant)
     y = Tensor(y, constant=y_constant)
     assert hasattr(Tensor, op), "`Tensor` is missing the attribute {}".format(op)
