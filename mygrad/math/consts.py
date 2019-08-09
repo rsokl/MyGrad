997666ea17e4c8d4822ac0ffc8e2f5a6e949dad9
Mikail Khan <mikail.khan45@gmail.com>
2019-08-09 14:44:34 -0400

    switched consts to numpy consts and numpy arrays

--- mygrad/math/consts.py
+++ mygrad/math/consts.py
@@ -1,10 +1,17 @@
-from mygrad.tensor_base import Tensor
+import numpy as np
 
-c = Tensor(2.99792458e9, constant=True) # speed of light in m/s
-e = Tensor(2.718281828459045, constant=True)
-euler_gamma = Tensor(0.5772156649015329, constant=True)
-G = Tensor(6.67384e-11, constant=True) # gravitational constant in (Nm^2)/(kg^2)
-h = Tensor(6.67384e-34, constant=True) # planck's constant in J*s
-pi = Tensor(3.141592653589793, constant=True)
-phi = Tensor(1.618033988749895, constant=True)
-tau = pi * 2
+Inf = np.array(np.Inf)
+Infinity = np.array(np.Infinity)
+NAN = np.array(np.NAN)
+NINF = np.array(np.NINF)
+NZERO = np.array(np.NZERO)
+NaN = np.array(np.NaN)
+PINF = np.array(np.PINF)
+PZERO = np.array(np.PZERO)
+e = np.array(np.e)
+euler_gamma = np.array(np.euler_gamma)
+inf = np.array(np.inf)
+infty = np.array(np.infty)
+nan = np.array(np.nan)
+newaxis = np.array(np.newaxis)
+pi = np.array(np.pi)
