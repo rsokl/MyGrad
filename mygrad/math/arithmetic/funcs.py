0346e5b1e40c224248456b33156ab3b1ba230cb7
Ryan Soklaski <ryan.soklaski@gmail.com>
2019-08-16 09:23:10 -0400

    Update mygrad/math/arithmetic/funcs.py
    Co-Authored-By: Petar Griggs <marrs2k@gmail.com>

--- mygrad/math/arithmetic/funcs.py
+++ mygrad/math/arithmetic/funcs.py
@@ -215,7 +215,7 @@ def multiply_sequence(*variables, constant=False):
 
 
 def add_sequence(*variables, constant=False):
-    """ ``f(a, b, ...) -> a * b * ...``
+    """ ``f(a, b, ...) -> a + b + ...``
 
     Multiply a sequence of N tensors.
 
