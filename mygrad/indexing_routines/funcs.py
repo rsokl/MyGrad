e66d902522a07cdd14128bb384839c119e7c986d
Ryan Soklaski <ry26099@mit.edu>
2019-11-30 21:29:15 -0500

    update docs

--- mygrad/indexing_routines/funcs.py
+++ mygrad/indexing_routines/funcs.py
@@ -25,7 +25,7 @@ def where(condition, x=not_set, y=not_set, constant=False):
     Return elements chosen from `x` or `y` depending on `condition`.
 
     .. note::
-        When only `condition` is provided, this function is a shorthand for
+        When only ``condition`` is provided, this function is a shorthand for
         ``np.asarray(condition).nonzero()``. The rest of this
         documentation covers only the case where all three arguments are
         provided.
@@ -44,6 +44,10 @@ def where(condition, x=not_set, y=not_set, constant=False):
     y : array_like
        Values from which to chosen where ``condition`` is ``False``.
 
+    constant : bool, optional(default=False)
+        If ``True``, the returned tensor is a constant (it
+        does not back-propagate a gradient)
+
     Returns
     -------
     out : mygrad.Tensor
@@ -55,33 +59,33 @@ def where(condition, x=not_set, y=not_set, constant=False):
     >>> import mygrad as mg
     >>> a = mg.arange(10)
     >>> a
-    array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    Tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     >>> mg.where(a < 5, a, 10*a)
     Tensor([ 0,  1,  2,  3,  4, 50, 60, 70, 80, 90])
 
-    This can be used on multidimensional arrays too:
+    This can be used on multidimensional tensors too:
 
     >>> mg.where([[True, False], [True, True]],
     ...          [[1, 2], [3, 4]],
     ...          [[9, 8], [7, 6]])
     Tensor([[1, 8],
-           [3, 4]])
+            [3, 4]])
 
     The shapes of x, y, and the condition are broadcast together:
 
     >>> x, y = np.ogrid[:3, :4]
     >>> mg.where(x < y, x, 10 + y)  # both x and 10+y are broadcast
     Tensor([[10,  0,  0,  0],
-           [10, 11,  1,  1],
-           [10, 11, 12,  2]])
+            [10, 11,  1,  1],
+            [10, 11, 12,  2]])
 
     >>> a = mg.Tensor([[0, 1, 2],
     ...                [0, 2, 4],
     ...                [0, 3, 6]])
     >>> mg.where(a < 4, a, -1)  # -1 is broadcast
     Tensor([[ 0,  1,  2],
-           [ 0,  2, -1],
-           [ 0,  3, -1]])
+            [ 0,  2, -1],
+            [ 0,  3, -1]])
     """
     if x is not_set and y is not_set:
         if isinstance(condition, Tensor):
--- mygrad/math/exp_log/funcs.py
+++ mygrad/math/exp_log/funcs.py
@@ -1,6 +1,6 @@
 from mygrad.tensor_base import Tensor
 
-from .ops import *
+from .ops import Exp, Exp2, Expm1, Log, Log1p, Log2, Log10, Logaddexp, Logaddexp2
 
 __all__ = [
     "exp",
@@ -18,201 +18,201 @@ __all__ = [
 def exp(a, constant=False):
     """ ``f(a) -> exp(a)``
 
-        Parameters
-        ----------
-        a : array_like
+    Parameters
+    ----------
+    a : array_like
 
-        constant : bool, optional(default=False)
-            If ``True``, the returned tensor is a constant (it
-            does not back-propagate a gradient)
+    constant : bool, optional(default=False)
+        If ``True``, the returned tensor is a constant (it
+        does not back-propagate a gradient)
 
-        Returns
-        -------
-        mygrad.Tensor"""
+    Returns
+    -------
+    mygrad.Tensor"""
     return Tensor._op(Exp, a, constant=constant)
 
 
 def exp2(a, constant=False):
     """``f(a) -> 2^a``
 
-        Parameters
-        ----------
-        a : array_like
+    Parameters
+    ----------
+    a : array_like
 
-        constant : bool, optional(default=False)
-            If ``True``, the returned tensor is a constant (it
-            does not back-propagate a gradient)
-        
-        Returns
-        -------
-        mygrad.Tensor"""
+    constant : bool, optional(default=False)
+        If ``True``, the returned tensor is a constant (it
+        does not back-propagate a gradient)
+
+    Returns
+    -------
+    mygrad.Tensor"""
     return Tensor._op(Exp2, a, constant=constant)
 
 
 def expm1(a, constant=False):
     """ ``f(a) -> exp(a) - 1``
 
-        The inverse of ``logp1``.
+    The inverse of ``logp1``.
 
-        Parameters
-        ----------
-        a : array_like
+    Parameters
+    ----------
+    a : array_like
 
-        constant : bool, optional(default=True)
-            If ``True``, the returned tensor is a constant (it
-            does not back-propagate a gradient)
+    constant : bool, optional(default=True)
+        If ``True``, the returned tensor is a constant (it
+        does not back-propagate a gradient)
 
-        Returns
-        -------
-        mygrad.Tensor
+    Returns
+    -------
+    mygrad.Tensor
 
-        Notes
-        -----
-        This function provides greater precision than ``exp(x) - 1`` for
-        small values of ``x``."""
+    Notes
+    -----
+    This function provides greater precision than ``exp(x) - 1`` for
+    small values of ``x``."""
     return Tensor._op(Expm1, a, constant=constant)
 
 
 def logaddexp(a, b, constant=False):
     """ ``f(a, b) -> log(exp(a) + exp(b))``
 
-        Parameters
-        ----------
-        a : array_like
+    Parameters
+    ----------
+    a : array_like
 
-        b : array_like
+    b : array_like
 
-        constant : bool, optional(default=False)
-            If ``True``, the returned tensor is a constant (it
-            does not back-propagate a gradient)
+    constant : bool, optional(default=False)
+        If ``True``, the returned tensor is a constant (it
+        does not back-propagate a gradient)
 
-        Returns
-        -------
-        mygrad.Tensor
+    Returns
+    -------
+    mygrad.Tensor
 
-        Notes
-        -----
-        This function is useful
-        in statistics where the calculated probabilities of events may
-        be so small as to exceed the range of normal floating point
-        numbers. In such cases the logarithm of the calculated
-        probability is stored. This function allows adding probabilities
-        stored in such a fashion. """
+    Notes
+    -----
+    This function is useful
+    in statistics where the calculated probabilities of events may
+    be so small as to exceed the range of normal floating point
+    numbers. In such cases the logarithm of the calculated
+    probability is stored. This function allows adding probabilities
+    stored in such a fashion. """
     return Tensor._op(Logaddexp, a, b, constant=constant)
 
 
 def logaddexp2(a, b, constant=False):
     """ ``f(a, b) -> log_2(2 ** a + 2 ** b)``
 
-        Utilizes base-2 log.
+    Utilizes base-2 log.
 
-        Parameters
-        ----------
-        a : array_like
+    Parameters
+    ----------
+    a : array_like
 
-        b : array_like
+    b : array_like
 
-        constant : bool, optional(default=False)
-            If ``True``, the returned tensor is a constant (it
-            does not back-propagate a gradient)
+    constant : bool, optional(default=False)
+        If ``True``, the returned tensor is a constant (it
+        does not back-propagate a gradient)
 
-        Returns
-        -------
-        mygrad.Tensor
+    Returns
+    -------
+    mygrad.Tensor
 
-        Notes
-        -----
-        This function is useful
-        in statistics where the calculated probabilities of events may
-        be so small as to exceed the range of normal floating point
-        numbers. In such cases the logarithm of the calculated
-        probability is stored. This function allows adding probabilities
-        stored in such a fashion. """
+    Notes
+    -----
+    This function is useful
+    in statistics where the calculated probabilities of events may
+    be so small as to exceed the range of normal floating point
+    numbers. In such cases the logarithm of the calculated
+    probability is stored. This function allows adding probabilities
+    stored in such a fashion. """
     return Tensor._op(Logaddexp2, a, b, constant=constant)
 
 
 def log(a, constant=False):
     """ ``f(a) -> log(a)``
 
-        Natural logarithm
+    Natural logarithm
 
-        Parameters
-        ----------
-        a : array_like
+    Parameters
+    ----------
+    a : array_like
 
-        constant : bool, optional(default=True)
-            If ``True``, the returned tensor is a constant (it
-            does not back-propagate a gradient)
+    constant : bool, optional(default=True)
+        If ``True``, the returned tensor is a constant (it
+        does not back-propagate a gradient)
 
-        Returns
-        -------
-        mygrad.Tensor
+    Returns
+    -------
+    mygrad.Tensor
 
-        Notes
-        -----
-        This function provides greater precision than ``exp(x) - 1`` for
-        small values of ``x``."""
+    Notes
+    -----
+    This function provides greater precision than ``exp(x) - 1`` for
+    small values of ``x``."""
     return Tensor._op(Log, a, constant=constant)
 
 
 def log2(a, constant=False):
     """ ``f(a) -> log2(a)``
 
-        Base-2 logarithm
+    Base-2 logarithm
 
-        Parameters
-        ----------
-        a : array_like
+    Parameters
+    ----------
+    a : array_like
 
-        constant : bool, optional(default=True)
-            If ``True``, the returned tensor is a constant (it
-            does not back-propagate a gradient)
+    constant : bool, optional(default=True)
+        If ``True``, the returned tensor is a constant (it
+        does not back-propagate a gradient)
 
-        Returns
-        -------
-        mygrad.Tensor"""
+    Returns
+    -------
+    mygrad.Tensor"""
     return Tensor._op(Log2, a, constant=constant)
 
 
 def log10(a, constant=False):
     """ ``f(a) -> log10(a)``
 
-        Base-10 logarithm
+    Base-10 logarithm
 
-        Parameters
-        ----------
-        a : array_like
+    Parameters
+    ----------
+    a : array_like
 
-        constant : bool, optional(default=True)
-            If ``True``, the returned tensor is a constant (it
-            does not back-propagate a gradient)
+    constant : bool, optional(default=True)
+        If ``True``, the returned tensor is a constant (it
+        does not back-propagate a gradient)
 
-        Returns
-        -------
-        mygrad.Tensor"""
+    Returns
+    -------
+    mygrad.Tensor"""
     return Tensor._op(Log10, a, constant=constant)
 
 
 def log1p(a, constant=False):
     """ f(a) -> log(1 + a)
 
-        The inverse of ``expm1``.
+    The inverse of ``expm1``.
 
-        Parameters
-        ----------
-        a : array_like
+    Parameters
+    ----------
+    a : array_like
 
-        constant : bool, optional(default=True)
-            If ``True``, the returned tensor is a constant (it
-            does not back-propagate a gradient)
+    constant : bool, optional(default=True)
+        If ``True``, the returned tensor is a constant (it
+        does not back-propagate a gradient)
 
-        Returns
-        -------
-        mygrad.Tensor
+    Returns
+    -------
+    mygrad.Tensor
 
-        Notes
-        -----
-        For real-valued input, log1p is accurate also
-        for ``x`` so small that ``1 + x == 1`` in floating-point
-        accuracy."""
+    Notes
+    -----
+    For real-valued input, log1p is accurate also
+    for ``x`` so small that ``1 + x == 1`` in floating-point
+    accuracy."""
     return Tensor._op(Log1p, a, constant=constant)
