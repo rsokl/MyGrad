d56ec47a16567f52852c288b343c21c75ac9bf59
Ryan Soklaski <ry26099@mit.edu>
2019-11-30 21:16:43 -0500

    add nygrad.where unit tests. Muck around with no-set for x/y

--- mygrad/indexing_routines/funcs.py
+++ mygrad/indexing_routines/funcs.py
@@ -7,9 +7,25 @@ from .ops import Where
 __all__ = ["where"]
 
 
-def where(condition, x=None, y=None, constant=False):
-    if x is None and y is None:
+class _UniqueIdentifier:
+    def __init__(self, identifier):
+        self.identifier = identifier
+
+    def __repr__(self):
+        return self.identifier
+
+
+not_set = _UniqueIdentifier("not_set")
+
+
+def where(condition, x=not_set, y=not_set, constant=False):
+    if x is not_set and y is not_set:
+        if isinstance(condition, Tensor):
+            condition = condition.data
         return np.where(condition)
+
+    if x is not_set or y is not_set:
+        raise ValueError("either both or neither of x and y should be given")
 
     return Tensor._op(
         Where, x, y, op_kwargs=dict(condition=condition), constant=constant
