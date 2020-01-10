67e2d41a222c63dcb7ff8a9c6ab64a94616eb6e8
David Mascharka <davidmascharka@gmail.com>
2020-01-10 14:27:29 -0800

    Fix sigmoid, add test

---
+++ tests/nnet/activations/test_sigmoid.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from mygrad.nnet.activations import sigmoid
+from tests.wrappers.uber import backprop_test_factory, fwdprop_test_factory
+
+
+@fwdprop_test_factory(
+    mygrad_func=sigmoid, true_func=lambda x: 1 / (1 + np.exp(-x)), num_arrays=1
+)
+def test_sigmoid_fwd():
+    pass
+
+
+def _not_large(*arrs, **kwargs):
+    x = arrs[0]
+    return np.all(np.abs(x.data) < 500)
+
+
+@backprop_test_factory(
+    mygrad_func=sigmoid, true_func=lambda x: 1 / (1 + np.exp(-x)), num_arrays=1, assumptions=_not_large
+)
+def test_sigmoid_bkwd():
+    pass
