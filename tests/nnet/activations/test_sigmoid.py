80a731115d6cf8477bcd4747bc48e536974344ed
Ryan Soklaski <ry26099@mit.edu>
2020-01-17 19:32:53 -0500

    fix bounds on sigmoid test

--- tests/nnet/activations/test_sigmoid.py
+++ tests/nnet/activations/test_sigmoid.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 
 from mygrad.nnet.activations import sigmoid
@@ -5,14 +7,20 @@ from tests.wrappers.uber import backprop_test_factory, fwdprop_test_factory
 
 
 @fwdprop_test_factory(
-    mygrad_func=sigmoid, true_func=lambda x: 1 / (1 + np.exp(-x)), num_arrays=1
+    mygrad_func=sigmoid,
+    true_func=lambda x: 1 / (1 + np.exp(-x)),
+    num_arrays=1,
+    index_to_bnds={0: (-np.log(sys.float_info.max), None)},
 )
 def test_sigmoid_fwd():
     pass
 
 
 @backprop_test_factory(
-    mygrad_func=sigmoid, true_func=lambda x: 1 / (1 + np.exp(-x)), num_arrays=1, index_to_bnds={0: (-500, 500)},
+    mygrad_func=sigmoid,
+    true_func=lambda x: 1 / (1 + np.exp(-x)),
+    num_arrays=1,
+    index_to_bnds={0: (-np.log(sys.float_info.max), None)},
 )
 def test_sigmoid_bkwd():
     pass
