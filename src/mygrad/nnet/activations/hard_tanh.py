0c0cc4472547637fece3c134650414998e1b7073
David Mascharka <davidmascharka@users.noreply.github.com>
2020-02-16 09:42:38 -0800

    Add hard_tanh activation (#195)
    * Fix sigmoid, add test
    
    * Add hard_tanh activation function
    
    * Use the correct bounds
    
    * Fix test, fix init
    
    * Update src/mygrad/nnet/activations/hard_tanh.py
    
    * add type checking and examples to hard-tanh
    
    Co-authored-by: Ryan Soklaski <ryan.soklaski@gmail.com>

---
+++ src/mygrad/nnet/activations/hard_tanh.py
@@ -0,0 +1,72 @@
+from numbers import Real
+
+from numpy import ndarray
+
+from mygrad.math.misc.funcs import maximum, minimum
+from mygrad.tensor_base import Tensor
+
+__all__ = ["hard_tanh"]
+
+
+def hard_tanh(x, *, lower_bound=-1, upper_bound=1, constant=False):
+    """ Returns the hard hyperbolic tangent function.
+
+    The hard_tanh function is `lower_bound` where `x` <= `lower_bound`, `upper_bound` where
+    `x` >= `upper_bound`, and `x` where `lower_bound` < `x` < `upper_bound`.
+
+    Parameters
+    ----------
+    x : array_like
+        The input, to which to apply the hard tanh function.
+
+    lower_bound : Real, optional (default=-1)
+        The lower bound on the hard tanh.
+
+    upper_bound : Real, optional (default=1)
+        The upper bound on the hard tanh.
+
+    constant : boolean, optional (default=False)
+        If ``True``, the returned tensor is a constant (it
+        does not back-propagate a gradient).
+
+    Returns
+    -------
+    mygrad.Tensor
+        The result of applying the "hard-tanh" function elementwise to `x`.
+
+    Examples
+    --------
+    >>> import mygrad as mg
+    >>> from mygrad.nnet.activations import hard_tanh
+    >>> x = mg.arange(-5, 6)
+    >>> x
+    Tensor([-5, -4, -3, -2, -1,  0,  1,  2,  3,  4,  5])
+    >>> y = hard_tanh(x, lower_bound=-3, upper_bound=3); y
+    Tensor([-3, -3, -3, -2, -1,  0,  1,  2,  3,  3,  3])
+    >>> y.backward()
+    >>> x.grad
+    array([0., 0., 0., 1., 1., 1., 1., 1., 0., 0., 0.])
+    """
+    if isinstance(lower_bound, (ndarray, Tensor)):
+        lower_bound = lower_bound.item()
+
+    if isinstance(upper_bound, (ndarray, Tensor)):
+        upper_bound = upper_bound.item()
+
+    if not isinstance(lower_bound, Real):
+        raise TypeError(
+            "`lower_bound` must be a real-valued scalar, got {} (type {})".format(
+                lower_bound, type(lower_bound)
+            )
+        )
+
+    if not isinstance(upper_bound, Real):
+        raise TypeError(
+            "`upper_bound` must be a real-valued scalar, got {} (type {})".format(
+                upper_bound, type(upper_bound)
+            )
+        )
+
+    return maximum(
+        lower_bound, minimum(x, upper_bound, constant=constant), constant=constant
+    )
---
+++ tests/nnet/activations/test_hard_tanh.py
@@ -0,0 +1,45 @@
+import hypothesis.strategies as st
+import numpy as np
+import pytest
+
+from mygrad.nnet.activations import hard_tanh
+from tests.wrappers.uber import backprop_test_factory, fwdprop_test_factory
+
+
+@pytest.mark.parametrize("lower_bound, upper_bound", [(None, 1), (1, None)])
+def test_input_validation(lower_bound, upper_bound):
+    with pytest.raises(TypeError):
+        hard_tanh(2, lower_bound=lower_bound, upper_bound=upper_bound)
+
+
+finite_floats = st.floats(allow_infinity=False, allow_nan=False)
+
+
+@fwdprop_test_factory(
+    mygrad_func=hard_tanh,
+    true_func=lambda x, lower_bound, upper_bound: np.maximum(
+        np.minimum(x, upper_bound), lower_bound
+    ),
+    num_arrays=1,
+    kwargs={
+        "lower_bound": lambda x: finite_floats | finite_floats.map(np.array),
+        "upper_bound": lambda x: finite_floats | finite_floats.map(np.array),
+    },
+)
+def test_hard_tanh_fwd():
+    pass
+
+
+@backprop_test_factory(
+    mygrad_func=hard_tanh,
+    true_func=lambda x, lower_bound, upper_bound: np.maximum(
+        np.minimum(x, upper_bound), lower_bound
+    ),
+    num_arrays=1,
+    kwargs={
+        "lower_bound": lambda x: finite_floats | finite_floats.map(np.array),
+        "upper_bound": lambda x: finite_floats | finite_floats.map(np.array),
+    },
+)
+def test_hard_tanh_bkwd():
+    pass
