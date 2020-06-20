aa3ec96cfc6c02ef93263f8834472129e52457b9
David Mascharka <davidmascharka@gmail.com>
2020-06-20 16:09:52 -0700

    Add example

--- src/mygrad/nnet/activations/soft_sign.py
+++ src/mygrad/nnet/activations/soft_sign.py
@@ -19,5 +19,17 @@ def soft_sign(x, constant=False):
     -------
     mygrad.Tensor
         The soft sign function applied to `x` elementwise.
+
+    Examples
+    --------
+    >>> import mygrad as mg
+    >>> from mygrad.nnet.activations import hard_tanh
+    >>> x = mg.arange(-5, 6)
+    >>> x
+    Tensor([-5, -4, -3, -2, -1,  0,  1,  2,  3,  4,  5])
+    >>> y = soft_sign(x); y
+    Tensor([-0.83333333, -0.8       , -0.75      , -0.66666667, -0.5       ,
+         0.        ,  0.5       ,  0.66666667,  0.75      ,  0.8       ,
+         0.83333333])
     """
     return divide(x, 1 + abs(x), constant=constant)
