219739186d76dfc0a8a814ffb2b61afa9669ba27
David Mascharka <davidmascharka@gmail.com>
2020-06-20 13:11:58 -0700

    Add leaky relu activation

--- src/mygrad/nnet/activations/__init__.py
+++ src/mygrad/nnet/activations/__init__.py
@@ -1,8 +1,17 @@
 from mygrad import tanh
 
 from .hard_tanh import hard_tanh
+from .leaky_relu import leaky_relu
 from .relu import relu
 from .sigmoid import sigmoid
 from .softmax import logsoftmax, softmax
 
-__all__ = ["hard_tanh", "logsoftmax", "relu", "sigmoid", "softmax", "tanh"]
+__all__ = [
+    "hard_tanh",
+    "leaky_relu",
+    "logsoftmax",
+    "relu",
+    "sigmoid",
+    "softmax",
+    "tanh",
+]
---
+++ tests/nnet/activations/test_leaky_relu.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+import hypothesis.strategies as st
+from mygrad.nnet.activations import leaky_relu
+from tests.wrappers.uber import backprop_test_factory, fwdprop_test_factory
+
+
+def _np_leaky_relu(x, slope):
+    return np.maximum(x, 0) + slope * np.minimum(x, 0)
+
+
+finite_floats = st.floats(allow_infinity=False, allow_nan=False, width=32)
+
+
+@fwdprop_test_factory(
+    mygrad_func=leaky_relu,
+    true_func=_np_leaky_relu,
+    num_arrays=1,
+    kwargs={"slope": lambda x: finite_floats | finite_floats.map(np.array)},
+)
+def test_leaky_relu_fwd():
+    pass
+
+
+def _away_from_zero(*arrs, **kwargs):
+    x = arrs[0]
+    return np.all(np.abs(x.data) > 1e-8)
+
+
+@backprop_test_factory(
+    mygrad_func=leaky_relu,
+    true_func=_np_leaky_relu,
+    num_arrays=1,
+    assumptions=_away_from_zero,
+    kwargs={"slope": lambda x: finite_floats | finite_floats.map(np.array)},
+)
+def test_leaky_relu_bkwd():
+    pass
