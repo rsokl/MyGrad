6b4cc8e05616a5d817c9ad8081b32216607d01d2
David Mascharka <davidmascharka@gmail.com>
2020-06-22 09:19:17 -0700

    Stop ELU raising overflow warnings

--- tests/nnet/activations/test_elu.py
+++ tests/nnet/activations/test_elu.py
@@ -1,3 +1,5 @@
+import sys
+
 import hypothesis.strategies as st
 import numpy as np
 import pytest
@@ -16,6 +18,7 @@ def test_input_validation(alpha):
 def _finite_params(arrs, alpha):
     if isinstance(arrs, Tensor):
         arrs = arrs.data
+
     return (
         np.all(np.isfinite(alpha * (np.exp(arrs) - 1)))
         and np.all(np.abs(np.exp(arrs)) > 1e-8)
@@ -26,14 +29,15 @@ def _np_elu(x, alpha):
     return np.where(x < 0, alpha * (np.exp(x) - 1), x)
 
 
-finite_floats = st.floats(allow_infinity=False, allow_nan=False)
+_reasonable_floats = st.floats(-100, 100)
 
 
 @fwdprop_test_factory(
     mygrad_func=elu,
     true_func=_np_elu,
     num_arrays=1,
-    kwargs={"alpha": lambda x: finite_floats | finite_floats.map(np.array)},
+    index_to_bnds={0: (-np.log(sys.float_info.max) / 100, np.log(sys.float_info.max) / 100)},
+    kwargs={"alpha": lambda x: _reasonable_floats | _reasonable_floats.map(np.array)},
     assumptions=_finite_params,
 )
 def test_elu_fwd():
@@ -44,8 +48,9 @@ def test_elu_fwd():
     mygrad_func=elu,
     true_func=_np_elu,
     num_arrays=1,
+    index_to_bnds={0: (-np.log(sys.float_info.max) / 100, np.log(sys.float_info.max) / 100)},
+    kwargs={"alpha": lambda x: _reasonable_floats | _reasonable_floats.map(np.array)},
     assumptions=_finite_params,
-    kwargs={"alpha": lambda x: finite_floats | finite_floats.map(np.array)},
 )
 def test_elu_bkwd():
     pass
