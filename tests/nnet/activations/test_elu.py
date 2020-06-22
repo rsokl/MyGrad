06551a2ee4c996eac9664ad2901be64e8773a873
David Mascharka <davidmascharka@gmail.com>
2020-06-22 09:29:19 -0700

    Cover tensor case

--- tests/nnet/activations/test_elu.py
+++ tests/nnet/activations/test_elu.py
@@ -4,6 +4,7 @@ import hypothesis.strategies as st
 import numpy as np
 import pytest
 
+from mygrad import Tensor
 from mygrad.nnet.activations import elu
 from tests.wrappers.uber import backprop_test_factory, fwdprop_test_factory
 
@@ -15,6 +16,8 @@ def test_input_validation(alpha):
 
 
 def _np_elu(x, alpha):
+    if isinstance(alpha, Tensor):
+        alpha = alpha.data
     return np.where(x < 0, alpha * (np.exp(x) - 1), x)
 
 
@@ -26,7 +29,7 @@ _reasonable_floats = st.floats(-100, 100)
     true_func=_np_elu,
     num_arrays=1,
     index_to_bnds={0: (-np.log(sys.float_info.max) / 100, np.log(sys.float_info.max) / 100)},
-    kwargs={"alpha": lambda x: _reasonable_floats | _reasonable_floats.map(np.array)},
+    kwargs={"alpha": lambda x: _reasonable_floats | _reasonable_floats.map(np.array) | _reasonable_floats.map(Tensor)},
 )
 def test_elu_fwd():
     pass
@@ -37,7 +40,7 @@ def test_elu_fwd():
     true_func=_np_elu,
     num_arrays=1,
     index_to_bnds={0: (-np.log(sys.float_info.max) / 100, np.log(sys.float_info.max) / 100)},
-    kwargs={"alpha": lambda x: _reasonable_floats | _reasonable_floats.map(np.array)},
+    kwargs={"alpha": lambda x: _reasonable_floats | _reasonable_floats.map(np.array) | _reasonable_floats.map(Tensor)},
 )
 def test_elu_bkwd():
     pass
