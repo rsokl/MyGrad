841a5f028b7fde6e890b4ec0657f906ecfa34f6f
David Mascharka <davidmascharka@gmail.com>
2020-06-22 09:25:43 -0700

    Remove now-unnecessary filter

--- tests/nnet/activations/test_selu.py
+++ tests/nnet/activations/test_selu.py
@@ -2,16 +2,9 @@ import sys
 
 import numpy as np
 
-from mygrad import Tensor
 from mygrad.nnet.activations import selu
 from mygrad.nnet.activations.selu import _ALPHA, _SCALE
 from tests.wrappers.uber import backprop_test_factory, fwdprop_test_factory
-
-
-def _finite_params(arrs):
-    if isinstance(arrs, Tensor):
-        arrs = arrs.data
-    return np.all(np.isfinite(_SCALE * _ALPHA * np.exp(arrs)))
 
 
 def _np_selu(x):
@@ -23,7 +16,6 @@ def _np_selu(x):
     true_func=_np_selu,
     num_arrays=1,
     index_to_bnds={0: (-np.log(sys.float_info.max), np.log(sys.float_info.max))},
-    assumptions=_finite_params,
 )
 def test_selu_fwd():
     pass
@@ -34,7 +26,6 @@ def test_selu_fwd():
     true_func=_np_selu,
     num_arrays=1,
     index_to_bnds={0: (-np.log(sys.float_info.max), np.log(sys.float_info.max))},
-    assumptions=_finite_params,
 )
 def test_selu_bkwd():
     pass
