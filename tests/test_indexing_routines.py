eac09b5d7ad02547316c59474cfb99fef2577ebf
Ryan Soklaski <ryan.soklaski@gmail.com>
2020-06-23 12:17:41 -0400

    fix bad test name (#233)

--- tests/test_indexing_routines.py
+++ tests/test_indexing_routines.py
@@ -60,7 +60,7 @@ def test_where_condition_only_fwd(condition):
     x=st.none() | hnp.arrays(shape=hnp.array_shapes(min_dims=1), dtype=int,),
     y=st.none() | hnp.arrays(shape=hnp.array_shapes(min_dims=1), dtype=int,),
 )
-def test_clip_input_validation(condition, x, y):
+def test_where_input_validation(condition, x, y):
     args = [i for i in (x, y) if i is not None]
 
     try:
