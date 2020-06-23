2534db503d6a4fcf500b3dd5fd22e66a89ed2481
David Mascharka <davidmascharka@gmail.com>
2020-06-23 15:20:22 -0700

    Cover input validation

--- tests/nnet/initializers/test_dirac.py
+++ tests/nnet/initializers/test_dirac.py
@@ -1,10 +1,16 @@
 from hypothesis import given
 import hypothesis.extra.numpy as hnp
-
 import numpy as np
+import pytest
 
 from mygrad import Tensor
 from mygrad.nnet.initializers import dirac
+
+
+@given(shape=hnp.array_shapes(max_dims=1))
+def test_dirac_bad_dimensions(shape):
+    with pytest.raises(ValueError):
+        dirac(shape)
 
 
 @given(shape=hnp.array_shapes(min_dims=2))
