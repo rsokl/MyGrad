a1a02f2c809f0871be98a7b30434e0168e91393a
David Mascharka <davidmascharka@users.noreply.github.com>
2020-06-25 09:09:24 -0700

    Add normal initializer (#252)

--- src/mygrad/nnet/initializers/__init__.py
+++ src/mygrad/nnet/initializers/__init__.py
@@ -1,9 +1,11 @@
 from mygrad.tensor_creation.funcs import identity
 from .constant import constant
 from .glorot_normal import glorot_normal
+from .normal import normal
 
 __all__ = [
     "constant",
     "glorot_normal",
     "identity",
+    "normal",
 ]
