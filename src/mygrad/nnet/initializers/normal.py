0a26c5e4e7aec8d33a9fa0547ad3c2ee5ddf20a2
David Mascharka <davidmascharka@gmail.com>
2020-06-25 08:59:02 -0700

    Add examples

--- src/mygrad/nnet/initializers/normal.py
+++ src/mygrad/nnet/initializers/normal.py
@@ -1,6 +1,7 @@
 import numpy as np
 from mygrad import Tensor
 
+
 def normal(*shape, mean=0, std=1, dtype=np.float32, constant=False):
     """ Initialize a :class:`mygrad.Tensor` by drawing from a normal (Gaussian) distribution.
 
@@ -26,6 +27,20 @@ def normal(*shape, mean=0, std=1, dtype=np.float32, constant=False):
     -------
     mygrad.Tensor, shape=`shape`
         A Tensor, with values drawn from Ɲ(μ, σ²), where μ=`mean` and σ=`std`.
+
+    Examples
+    --------
+    >>> from mygrad.nnet.initializers import normal
+    >>> normal(1, 2, 3)
+    Tensor([[[-0.06481607, -0.550582  ,  0.04689528],
+         [ 0.82973075,  2.83742   ,  1.0964519 ]]], dtype=float32)
+
+    >>> normal(2, 2, dtype="float16", constant=True)
+    Tensor([[-1.335 ,  0.9297],
+        [ 1.746 , -0.1222]], dtype=float16)
+
+    >>> normal(5, dtype="float64")
+    Tensor([-0.03875407,  0.65368466, -0.72636993,  1.57404148, -1.17444345])
     """
     if not np.issubdtype(dtype, np.floating):
         raise ValueError("Glorot Normal initialization requires a floating-point dtype")
