5d5b56549a2658505e209801f91fd8f2b4aa3916
David Mascharka <davidmascharka@users.noreply.github.com>
2020-06-25 10:02:51 -0700

    Add uniform initializer (#251)

--- src/mygrad/nnet/initializers/__init__.py
+++ src/mygrad/nnet/initializers/__init__.py
@@ -2,10 +2,12 @@ from mygrad.tensor_creation.funcs import identity
 from .constant import constant
 from .glorot_normal import glorot_normal
 from .normal import normal
+from .uniform import uniform
 
 __all__ = [
     "constant",
     "glorot_normal",
     "identity",
     "normal",
+    "uniform",
 ]
