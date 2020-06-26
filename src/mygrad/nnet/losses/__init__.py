ee522f1438fe5c06f6bfe41177d648facff7bb19
David Mascharka <davidmascharka@gmail.com>
2020-06-25 17:45:07 -0700

    Add focal loss

--- src/mygrad/nnet/losses/__init__.py
+++ src/mygrad/nnet/losses/__init__.py
@@ -1,5 +1,12 @@
+from .focal_loss import focal_loss, softmax_focal_loss
 from .margin_ranking_loss import margin_ranking_loss
 from .multiclass_hinge import multiclass_hinge
 from .softmax_crossentropy import softmax_crossentropy
 
-__all__ = ["margin_ranking_loss", "multiclass_hinge", "softmax_crossentropy"]
+__all__ = [
+    "focal_loss",
+    "margin_ranking_loss",
+    "multiclass_hinge",
+    "softmax_crossentropy",
+    "softmax_focal_loss",
+]
