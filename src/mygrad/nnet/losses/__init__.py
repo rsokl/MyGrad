7d4b74d640baa30a9ee2357045e3e228df97e235
David Mascharka <davidmascharka@users.noreply.github.com>
2020-06-25 17:45:46 -0700

    Update __init__.py

--- src/mygrad/nnet/losses/__init__.py
+++ src/mygrad/nnet/losses/__init__.py
@@ -3,4 +3,9 @@ from .multiclass_hinge import multiclass_hinge
 from .negative_log_likelihood import negative_log_likelihood
 from .softmax_crossentropy import softmax_crossentropy
 
-__all__ = ["margin_ranking_loss", "multiclass_hinge", "softmax_crossentropy"]
+__all__ = [
+  "margin_ranking_loss",
+  "multiclass_hinge",
+  "negative_log_likelihood",
+  "softmax_crossentropy",
+]
