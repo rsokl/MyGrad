a7783b1f9019cec80393daa16896204a4d16882b
Darshan Krishnaswamy <darsh797@gmail.com>
2020-07-23 19:22:04 -0400

    Simplified testing using pytest parametrizing

--- tests/random/test_random.py
+++ tests/random/test_random.py
@@ -3,6 +3,7 @@ import hypothesis.strategies as st
 import numpy as np
 from hypothesis import given
 from numpy.testing import assert_allclose
+import pytest
 
 from mygrad.random import *
 from mygrad.tensor_base import Tensor
@@ -11,17 +12,12 @@ dtype_strat_numpy = st.sampled_from(
     (np.int8, np.int16, np.int32, np.int64, np.float16, np.float32, np.float64)
 )
 
-
-@given(
-    a=hnp.arrays(
-        shape=hnp.array_shapes(max_side=4, max_dims=5), dtype=dtype_strat_numpy
-    )
-)
-def test_rand(a):
-    shape = a.shape
-    b = rand(*shape)
-    assert b.shape == a.shape
-    assert (0 <= b.data).all() and (b.data <= 1).all()
+shape_functions = [
+    (np.random.sample, sample),
+    (np.random.random_sample, random_sample),
+    (np.random.ranf, ranf),
+    (np.random.random, random),
+]
 
 
 @given(
@@ -29,10 +25,38 @@ def test_rand(a):
         shape=hnp.array_shapes(max_side=4, max_dims=5), dtype=dtype_strat_numpy
     )
 )
-def test_randn(a):
+@pytest.mark.parametrize("np_function,mg_function", shape_functions)
+def test_random_shape_funcs(np_function, mg_function, a):
     shape = a.shape
-    b = randn(*shape)
-    assert b.shape == a.shape
+    np.random.seed(0)
+    arr = np_function(shape)
+    np.random.seed(0)
+    tens = mg_function(shape)
+    assert_allclose(arr, tens.data)
+
+
+unpacked_shape_functions = [(np.random.rand, rand), (np.random.randn, randn)]
+
+
+@given(
+    a=hnp.arrays(
+        shape=hnp.array_shapes(max_side=4, max_dims=5), dtype=dtype_strat_numpy
+    )
+)
+@pytest.mark.parametrize("np_function,mg_function", unpacked_shape_functions)
+def test_random_shape_funcs(np_function, mg_function, a):
+    shape = a.shape
+    np.random.seed(0)
+    arr = np_function(*shape)
+    np.random.seed(0)
+    tens = mg_function(*shape)
+    assert_allclose(arr, tens.data)
+
+
+bound_shape_functions = [
+    (np.random.randint, randint),
+    (np.random.random_integers, random_integers),
+]
 
 
 @given(
@@ -42,78 +66,16 @@ def test_randn(a):
     m=st.integers(-10000, 10000),
     n=st.integers(-10000, 10000),
 )
-def test_randint(a, m, n):
+@pytest.mark.parametrize("np_function,mg_function", bound_shape_functions)
+def test_bound_shape_functions(np_function, mg_function, m, n, a):
+    shape = a.shape
     shape = a.shape
     if m > n:
         m, n = n, m
     elif m == n:
         n = n + 1
-    b = randint(m, n, shape)
-    assert b.shape == a.shape
-    assert (m <= b.data).all() and (b.data < n).all()
-
-
-@given(
-    a=hnp.arrays(
-        shape=hnp.array_shapes(max_side=4, max_dims=5), dtype=dtype_strat_numpy
-    )
-)
-def test_random(a):
-    shape = a.shape
-    b = random(shape)
-    assert b.shape == a.shape
-    assert (0 <= b.data).all() and (b.data <= 1).all()
-
-
-@given(
-    a=hnp.arrays(
-        shape=hnp.array_shapes(max_side=4, max_dims=5), dtype=dtype_strat_numpy
-    ),
-    m=st.integers(-10000, 10000),
-    n=st.integers(-10000, 10000),
-)
-def test_random_integers(m, n, a):
-    shape = a.shape
-    if m > n:
-        m, n = n, m
-    elif m == n:
-        n = n + 1
-    b = random_integers(m, n, shape)
-    assert b.shape == a.shape
-    assert (m <= b.data).all() and (b.data <= n).all()
-
-
-@given(
-    a=hnp.arrays(
-        shape=hnp.array_shapes(max_side=4, max_dims=5), dtype=dtype_strat_numpy
-    )
-)
-def test_random_sample(a):
-    shape = a.shape
-    b = random_sample(shape)
-    assert b.shape == a.shape
-    assert (0 <= b.data).all() and (b.data <= 1).all()
-
-
-@given(
-    a=hnp.arrays(
-        shape=hnp.array_shapes(max_side=4, max_dims=5), dtype=dtype_strat_numpy
-    )
-)
-def test_ranf(a):
-    shape = a.shape
-    b = ranf(shape)
-    assert b.shape == a.shape
-    assert (0 <= b.data).all() and (b.data <= 1).all()
-
-
-@given(
-    a=hnp.arrays(
-        shape=hnp.array_shapes(max_side=4, max_dims=5), dtype=dtype_strat_numpy
-    )
-)
-def test_sample(a):
-    shape = a.shape
-    b = sample(shape)
-    assert b.shape == a.shape
-    assert (0 <= b.data).all() and (b.data <= 1).all()
+    np.random.seed(0)
+    arr = np_function(m, n, shape)
+    np.random.seed(0)
+    tens = mg_function(m, n, shape)
+    assert_allclose(arr, tens.data)
