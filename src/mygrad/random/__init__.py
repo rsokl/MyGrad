28981bb115fb31f1dfa41cb1330cfdd4e4f67665
Darshan Krishnaswamy <darsh797@gmail.com>
2020-07-24 13:05:20 -0400

    Updated directory structure

--- src/mygrad/random/__init__.py
+++ src/mygrad/random/__init__.py
@@ -1,1 +1,13 @@
-from .random import *
+from .funcs import *
+
+__all__ = [
+    "rand",
+    "randint",
+    "randn",
+    "random",
+    "random_integers",
+    "random_sample",
+    "ranf",
+    "sample",
+    "seed",
+]
