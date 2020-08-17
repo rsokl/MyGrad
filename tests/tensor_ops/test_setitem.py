fda7c0178765d4cbef0b2ce25308ccc7108ef52f
Ryan Soklaski <ry26099@mit.edu>
2020-08-17 00:53:12 -0400

    add test against arbitrary indices

--- tests/tensor_ops/test_setitem.py
+++ tests/tensor_ops/test_setitem.py
@@ -14,6 +14,7 @@ from mygrad.tensor_core_ops.indexing import (
     _is_bool_array_index,
     _is_int_array_index,
 )
+from tests.custom_strategies import arbitrary_indices
 
 from ..custom_strategies import adv_integer_index, basic_indices, broadcastable_shapes
 from ..utils.numerical_gradient import numerical_gradient_full
@@ -60,6 +61,71 @@ def setitem(x, y, index):
     x_copy = np.copy(x)
     x_copy[index] = y
     return x_copy
+
+
+class set_item_test_factory:
+    def __init__(
+        self,
+        array_strat: st.SearchStrategy[np.ndarray],
+        index_strat: Callable[[np.ndarray], st.SearchStrategy],
+        value_strat: Callable[[np.ndarray], st.SearchStrategy[np.ndarray]],
+    ):
+        self.array_strat = array_strat
+        self.index_strat = index_strat
+        self.value_strat = value_strat
+
+    def __call__(self, f):
+        @given(
+            data=st.data(), x=self.array_strat,
+        )
+        @wraps(f)
+        def wrapper(x: np.ndarray, data: st.DataObject):
+
+            index = data.draw(self.index_strat(x), label="index")
+
+            try:
+                o = np.asarray(x[index])
+            except IndexError:
+                assume(False)
+                return
+
+            note("x[index]: {}".format(o))
+            y = data.draw(self.value_strat(o), label="y",)
+
+            x0 = np.copy(x)
+            y0 = np.copy(y)
+
+            x_arr = Tensor(np.copy(x))
+            y_arr = Tensor(np.copy(y))
+            x1_arr = +x_arr
+
+            try:
+                x0[index] = y0  # don't permit invalid set-items
+            except Exception:
+                assume(False)
+                return
+
+            grad = data.draw(
+                hnp.arrays(
+                    shape=x.shape, dtype=float, elements=st.floats(1, 10), unique=True
+                ),
+                label="grad",
+            )
+
+            x1_arr[index] = y_arr
+            (x1_arr * grad).sum().backward()
+
+            assert_allclose(x1_arr.data, x0)
+            assert_allclose(y_arr.data, y0)
+
+            dx, dy = numerical_gradient_full(
+                setitem, x, y, back_grad=grad, kwargs=dict(index=index)
+            )
+
+            assert_allclose(x_arr.grad, dx)
+            assert_allclose(y_arr.grad, dy)
+
+        return wrapper
 
 
 def test_setitem_multiple_input():
@@ -160,75 +226,10 @@ def test_no_mutate():
     assert_allclose(np.array([0.0, 0.0]), y.data)
 
 
-class set_item_test_factory:
-    def __init__(
-        self,
-        array_strat: st.SearchStrategy[np.ndarray],
-        index_strat: Callable[[np.ndarray], st.SearchStrategy],
-        value_strat: Callable[[np.ndarray], st.SearchStrategy[np.ndarray]],
-    ):
-        self.array_strat = array_strat
-        self.index_strat = index_strat
-        self.value_strat = value_strat
-
-    def __call__(self, f):
-        @given(
-            data=st.data(), x=self.array_strat,
-        )
-        @wraps(f)
-        def wrapper(x: np.ndarray, data: st.DataObject):
-
-            index = data.draw(self.index_strat(x), label="index")
-
-            try:
-                o = np.asarray(x[index])
-            except IndexError:
-                assume(False)
-                return
-
-            note("x[index]: {}".format(o))
-            y = data.draw(self.value_strat(o), label="y",)
-
-            x0 = np.copy(x)
-            y0 = np.copy(y)
-
-            x_arr = Tensor(np.copy(x))
-            y_arr = Tensor(np.copy(y))
-            x1_arr = +x_arr
-
-            try:
-                x0[index] = y0  # don't permit invalid set-items
-            except ValueError:
-                assume(False)
-                return
-
-            grad = data.draw(
-                hnp.arrays(
-                    shape=x.shape, dtype=float, elements=st.floats(1, 10), unique=True
-                ),
-                label="grad",
-            )
-
-            x1_arr[index] = y_arr
-            (x1_arr * grad).sum().backward()
-
-            assert_allclose(x1_arr.data, x0)
-            assert_allclose(y_arr.data, y0)
-
-            dx, dy = numerical_gradient_full(
-                setitem, x, y, back_grad=grad, kwargs=dict(index=index)
-            )
-
-            assert_allclose(x_arr.grad, dx)
-            assert_allclose(y_arr.grad, dy)
-
-        return wrapper
-
-
 @settings(deadline=None, max_examples=1000)
 @set_item_test_factory(
     array_strat=hnp.arrays(
-        shape=hnp.array_shapes(max_side=4, max_dims=5),
+        shape=hnp.array_shapes(min_side=0, max_side=4, min_dims=0, max_dims=5),
         dtype=float,
         elements=st.floats(-10.0, 10.0),
     ),
@@ -247,7 +248,7 @@ class set_item_test_factory:
             elements=st.floats(-10.0, 10.0),
         )
         if o.shape and o.size
-        else st.floats(-10.0, 10.0).map(lambda _x: np.array(_x))
+        else st.floats(-10.0, 10.0).map(np.array)
     ),
 )
 def test_setitem_basic_index():
@@ -384,4 +385,34 @@ def test_setitem_bool_basic_index():
 )
 def test_setitem_bool_axes_index():
     """ index consists of boolean arrays specified for each axis """
+    pass
+
+
+@settings(deadline=None, max_examples=1000)
+@set_item_test_factory(
+    array_strat=hnp.arrays(
+        shape=hnp.array_shapes(min_side=0, max_side=4, min_dims=0, max_dims=5),
+        dtype=float,
+        elements=st.floats(-10.0, 10.0),
+    ),
+    index_strat=lambda x: arbitrary_indices(x.shape),
+    value_strat=lambda o: (
+        hnp.arrays(
+            # Permit shapes that are broadcast-compatible with x[index]
+            # The only excess dimensions permitted in this shape are
+            # leading singletons
+            shape=broadcastable_shapes(o.shape).map(
+                lambda _x: tuple(
+                    1 if (len(_x) - n) > o.ndim else s for n, s in enumerate(_x)
+                )
+            ),
+            dtype=float,
+            elements=st.floats(-10.0, 10.0),
+        )
+        if o.shape and o.size
+        else st.floats(-10.0, 10.0).map(np.array)
+    ),
+)
+def test_setitem_arbitrary_index():
+    """ test arbitrary indices"""
     pass
