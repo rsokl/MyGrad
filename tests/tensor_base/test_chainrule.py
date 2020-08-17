3c49595a49c6588d2e5d37ba4e5889e6510a3feb
Ryan Soklaski <ry26099@mit.edu>
2020-08-17 14:16:38 -0400

    add fanout graph

--- tests/tensor_base/test_chainrule.py
+++ tests/tensor_base/test_chainrule.py
@@ -125,7 +125,7 @@ def test_linear_graph(
     v4_const: bool,
     grad: float,
 ):
-    """
+    r"""
      v1
      |
      v2
@@ -142,7 +142,7 @@ def test_linear_graph(
     note(f"v1: {v1}")
     note(f"v2: {v2}")
     note(f"v3: {v3}")
-    note(f"v3: {v4}")
+    note(f"v4: {v4}")
 
     v4.backward(grad)
 
@@ -158,26 +158,126 @@ def test_linear_graph(
     assert v4.constant is (v4_const or v3.constant or v2.constant or v1.constant)
 
     # check that gradients are correct
+    # dL/d4
     _check_grad(v4, None if v4.constant else grad)
-    _check_grad(v3, None if v4.constant else 2 * grad)
+    # dL/d3 = dL/d4 * d4/d3
+    _check_grad(v3, None if v4.constant else grad * 2)
+    # dL/d2 = dL/d4 * d4/d3 * d3/d2
     _check_grad(
-        v2, None if (v4.constant or v3.constant) else 2 * grad * np.exp(v2.data)
+        v2, None if (v4.constant or v3.constant) else grad * (2 * np.exp(v2.data))
     )
+    # dL/d2 = dL/d4 * d4/d3 * d3/d2 * d2/d1
     _check_grad(
         v1,
         None
         if (v4.constant or v3.constant or v2.constant)
-        else 4 * grad * v1_val * np.exp(v2.data),
+        else grad * (2 * np.exp(v2.data)) * (2 * v1.data),
     )
 
     # check that backprop metadata cleared appropriately upon completion of backprop
+    assert not v4._accum_ops and v4.creator is not None
     assert not v3._accum_ops and v3.creator is not None
     assert not v2._accum_ops and v2.creator is not None
     assert not v1._accum_ops and v1.creator is None
 
-    # check the null grads & clear graph always ropagates through the graph
+    # check the null grads & clear graph always propagates through the graph
     v4.null_gradients(clear_graph=True)
     assert v4.grad is None and v4.creator is None
+    assert v3.grad is None and not v3._ops and v3.creator is None
+    assert v2.grad is None and not v2._ops and v2.creator is None
+    assert v1.grad is None and not v1._ops and v1.creator is None
+
+
+@pytest.mark.parametrize("v1_const", [True, False])
+@pytest.mark.parametrize("v2_const", [True, False])
+@pytest.mark.parametrize("v3_const", [True, False])
+@pytest.mark.parametrize("v4_const", [True, False])
+@pytest.mark.parametrize("v5_const", [True, False])
+@given(
+    v1_val=st.integers(-2, 2).map(float), grad=st.integers(-2, 2).map(float),
+)
+def test_fanout_graph(
+    v1_val: float,
+    v1_const: bool,
+    v2_const: bool,
+    v3_const: bool,
+    v4_const: bool,
+    v5_const: bool,
+    grad: float,
+):
+    r"""
+      v1
+    / | \
+   v2 v3 v4
+    \ | /
+     v5
+    """
+    v1 = Tensor(v1_val, constant=v1_const)
+    v2 = mg.square(v1, constant=v2_const)
+    v3 = mg.exp(v1, constant=v3_const)
+    v4 = mg.multiply(v1, 2.0, constant=v4_const)
+    v5 = mg.multiply_sequence(v2, v3, v4, constant=v5_const)
+
+    note(f"v1: {v1}")
+    note(f"v2: {v2}")
+    note(f"v3: {v3}")
+    note(f"v4: {v4}")
+    note(f"v5: {v5}")
+
+    v5.backward(grad)
+
+    # check fwd-pass produces reliable math
+    assert v2.data == v1_val ** 2
+    assert v3.data == np.exp(v1_val)
+    assert v4.data == 2 * v1_val
+    assert v5.data == v2.data * v3.data * v4.data
+
+    # check that constant propagates through graph reliably
+    assert v1.constant is v1_const
+    assert v2.constant is (v2_const or v1_const)
+    assert v3.constant is (v3_const or v1_const)
+    assert v4.constant is (v4_const or v1_const)
+    assert v5.constant is v5_const or v1_const or (v2_const and v3_const and v4_const)
+
+    # check that gradients are correct
+    # dL/d5
+    _check_grad(v5, grad)
+    # dL/d4 = dL/d5 * p5/p4
+    _check_grad(v4, None if v5_const else grad * (v2.data * v3.data))
+    # dL/d3 = dL/d5 * p5/p3
+    _check_grad(v3, None if v5_const else grad * (v2.data * v4.data))
+    # dL/d2 = dL/d5 * p5/p2
+    _check_grad(v2, None if v5_const else grad * (v3.data * v4.data))
+
+    v1_grad = False
+    # dL/d1 = +? dL/d2*p2/p1 +? dL/d3*p3/p1 +? dL/d4*p4/p1
+    if not v5_const and not v2.constant:
+        v1_grad += v2.grad * 2 * v1_val
+
+    if not v5_const and not v3.constant:
+        v1_grad += v3.grad * np.exp(v1_val)
+
+    if not v5_const and not v4.constant:
+        v1_grad += v4.grad * 2
+
+    if v1_grad is False:
+        v1_grad = None
+
+    _check_grad(
+        v1, None if v5_const or (v2_const and v3_const and v4_const) else v1_grad
+    )
+
+    # check that backprop metadata cleared appropriately upon completion of backprop
+    assert not v5._accum_ops and v5.creator is not None
+    assert not v4._accum_ops and v4.creator is not None
+    assert not v3._accum_ops and v3.creator is not None
+    assert not v2._accum_ops and v2.creator is not None
+    assert not v1._accum_ops and v1.creator is None
+
+    # check the null grads & clear graph always propagates through the graph
+    v5.null_gradients(clear_graph=True)
+    assert v5.grad is None and v5.creator is None
+    assert v4.grad is None and not v4._ops and v4.creator is None
     assert v3.grad is None and not v3._ops and v3.creator is None
     assert v2.grad is None and not v2._ops and v2.creator is None
     assert v1.grad is None and not v1._ops and v1.creator is None
@@ -203,7 +303,7 @@ def test_interesting_graph(
     v5_const: bool,
     grad: float,
 ):
-    """
+    r"""
              v1
              /\
             ---
@@ -244,19 +344,25 @@ def test_interesting_graph(
     assert v5.constant is v5_const or (v3.constant and v4.constant)
 
     # check that gradients are correct
+    # dL/d5
     _check_grad(v5, None if v5.constant else grad)
+
+    # dL/d4 = dL/d5 * p5/p4
     _check_grad(v4, None if v5.constant else grad * v3.data)
 
+    # dL/d3 = dL/d5 * p5/p3 +? dL/d4 * p4/p3
     v3_grad = (
         None
         if v5.constant
-        else (grad * v4.data if v4.constant else 2 * grad * v2.data * v3.data)
+        else (grad * v4.data if v4.constant else grad * (2 * v3.data) * v2.data)
     )
     _check_grad(v3, v3_grad)
 
+    # dL/d2 = dL/d4 * p4/p2
     v2_grad = None if (v5.constant or v4.constant) else grad * v3.data ** 2
     _check_grad(v2, v2_grad)
 
+    # dL/d1 = dL/d3 * p3/p1
     v1_grad = (
         None
         if (v5.constant or v3.constant)
@@ -275,7 +381,7 @@ def test_interesting_graph(
     assert not v2._accum_ops and v2.creator is None
     assert not v1._accum_ops and v1.creator is None
 
-    # check the null grads & clear graph always ropagates through the graph
+    # check the null grads & clear graph always propagates through the graph
     v5.null_gradients(clear_graph=True)
     assert v5.grad is None and v5.creator is None
     assert v4.grad is None and not v4._ops and v4.creator is None
