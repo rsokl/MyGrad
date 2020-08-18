6d52de4a2151ed38b55963986903dc60fa00c8d7
Ryan Soklaski <ry26099@mit.edu>
2020-08-18 18:12:10 -0400

    move asarray definition to tensor_base to avoid import cycles

--- src/mygrad/tensor_base.py
+++ src/mygrad/tensor_base.py
@@ -28,7 +28,81 @@ from mygrad.tensor_core_ops.indexing import GetItem, SetItem
 from mygrad.tensor_manip.array_shape.ops import Flatten, Reshape
 from mygrad.tensor_manip.transpose_like.ops import Tensor_Transpose_Property
 
-__all__ = ["Tensor"]
+__all__ = ["Tensor", "asarray"]
+
+
+def asarray(a, dtype=None, order=None) -> np.ndarray:
+    """Convert the input to an array.
+
+    This docstring is adapted from that of ``numpy.asarray``
+
+    Parameters
+    ----------
+    a : array_like
+        Input data, in any form - including a mygrad tensor - that can be converted to an array. This
+        includes lists, lists of tuples, tuples, tuples of tuples, tuples
+        of lists and ndarrays.
+
+    dtype : data-type, optional
+        By default, the data-type is inferred from the input data.
+
+    order : {'C', 'F'}, optional
+        Whether to use row-major (C-style) or
+        column-major (Fortran-style) memory representation.
+        Defaults to 'C'.
+
+    Returns
+    -------
+    out : ndarray
+        Array interpretation of `a`.  No copy is performed if the input
+        is already an ndarray with matching dtype and order.  If `a` is a
+        subclass of ndarray, a base class ndarray is returned.
+
+    Examples
+    --------
+    Convert a list into an array:
+
+    >>> import mygrad as mg
+    >>> a = [1, 2]
+    >>> mg.asarray(a)
+    array([1, 2])
+
+    Convert a tensor into an array. No copy of the
+    underlying numpy array is created:
+
+    >>> t = mg.Tensor([1, 2.])
+    >>> mg.asarray(t)
+    array([1., 2.])
+    >>> t.data is np.asarray(t))
+    True
+
+    Existing arrays are not copied:
+
+    >>> a = np.array([1, 2])
+    >>> mg.asarray(a) is a
+    True
+
+    If `dtype` is set, array is copied only if dtype does not match:
+
+    >>> a = np.array([1, 2], dtype=np.float32)
+    >>> mg.asarray(a, dtype=np.float32) is a
+    True
+    >>> mg.asarray(a, dtype=np.float64) is a
+    False
+
+    Contrary to `asanyarray`, ndarray subclasses are not passed through:
+
+    >>> issubclass(np.recarray, np.ndarray)
+    True
+    >>> a = np.array([(1.0, 2), (3.0, 4)], dtype='f4,i4').view(np.recarray)
+    >>> mg.asarray(a) is a
+    False
+    >>> np.asanyarray(a) is a
+    True
+    """
+    if isinstance(a, Tensor):
+        a = a.data
+    return np.asarray(a, dtype=dtype, order=order)
 
 
 class Tensor:
@@ -331,7 +405,7 @@ class Tensor:
             return
 
         if grad is not None:
-            self.grad = np.asarray(grad.data if isinstance(grad, Tensor) else grad)
+            self.grad = asarray(grad)
             if is_invalid_gradient(self.grad):
                 raise InvalidGradient(
                     f"An invalid gradient-value was passed to "
@@ -982,10 +1056,14 @@ class Tensor:
         return self._op(Tensor_Transpose_Property, self)
 
     def __eq__(self, other):
-        return np.ndarray.__eq__(self.data, other.data if isinstance(other, Tensor) else other)
+        return np.ndarray.__eq__(
+            self.data, other.data if isinstance(other, Tensor) else other
+        )
 
     def __ne__(self, other):
-        return np.ndarray.__ne__(self.data, other.data if isinstance(other, Tensor) else other)
+        return np.ndarray.__ne__(
+            self.data, other.data if isinstance(other, Tensor) else other
+        )
 
 
 # set all comparison operators - mirrors ndarray methods
