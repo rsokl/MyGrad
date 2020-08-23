47518ab1408286b7f3612a89fd970c72938a13dd
Ryan Soklaski <ry26099@mit.edu>
2020-08-23 15:10:37 -0400

    do matmul on actual tensor

--- tests/tensor_base/test_augmented_updates.py
+++ tests/tensor_base/test_augmented_updates.py
@@ -117,10 +117,10 @@ def test_augmented_power():
 
 def test_augmented_matmul():
     a = np.arange(9.0).reshape(3, 3)
-    t = np.arange(9.0).reshape(3, 3)
+    t = mg.arange(9.0).reshape(3, 3)
 
     try:
         a[a < 4] @= np.arange(4.0)
     except Exception as e:
         with pytest.raises(type(e), match="Use 'a = a @ b' instead of 'a @= b'"):
-            t[a < 4] @= mg.arange(4.0)
+            t[t < 4] @= mg.arange(4.0)
