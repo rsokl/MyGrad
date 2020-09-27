bb0a1e09166c1681724781b18b5851edf26eb2ad
Ryan Soklaski <ry26099@mit.edu>
2020-09-27 16:46:55 -0400

    add more tests

--- tests/test_in_place_semantics.py
+++ tests/test_in_place_semantics.py
@@ -1,9 +1,10 @@
 from typing import Callable
 
 import hypothesis.strategies as st
+import numpy as np
 import pytest
 from hypothesis import given
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_allclose, assert_array_equal
 
 import mygrad as mg
 from mygrad import Tensor
@@ -26,6 +27,65 @@ def test_in_place_op_propagates_to_views(constant: bool):
     assert_array_equal(y, [-1.0, -1.0, 3.0, 4.0])
     assert_array_equal(y, view1)
     assert_array_equal(y, view2)
+
+
+@given(tensors(shape=(4,), constant=False))
+def test_simple_backprop_from_view_post_upstream_mutation(x: Tensor):
+    y = +x
+    z = y[...]
+    y[:2] = 0  # base is mutated
+    # downstream view should carry appropriate info
+    # for backprop post-mutation
+    z.backward()
+
+    assert_array_equal(z, y)
+    assert_array_equal(z.grad, np.ones_like(y))
+    assert_array_equal(y.grad, np.ones_like(y))
+    assert_array_equal(x.grad, [0.0, 0.0, 1.0, 1.0])
+
+
+@given(tensors(shape=(4,), elements=st.floats(-10, 10), constant=False))
+def test_mutation_doesnt_corrupt_upstream_op(x: Tensor):
+    y = +x
+    view = y[...]
+
+    # z = x**4
+    z = mg.multiply_sequence(x, y, view, view[...])
+
+    y[:2] = 0  # shouldn't change backprop through z
+
+    z.backward()  # dz/dx = 6 * x ** 2
+
+    assert_allclose(z, x.data ** 4)
+    assert_array_equal(view, y)
+    assert_allclose(z.grad, np.ones_like(y))
+    assert_allclose(x.grad, 4 * np.asarray(x) ** 3)
+
+    assert y.grad is None
+    assert view.grad is None
+
+
+@pytest.mark.parametrize("constant", [True, False])
+def test_sets_and_restores_writeability(constant: bool):
+    x = mg.arange(1.0, 5.0, constant=constant)
+    y = +x
+    y[...] = 0
+    assert x.data.flags.writeable is False
+    assert y.data.flags.writeable is False
+    y.backward()
+    assert x.data.flags.writeable is True
+    assert y.data.flags.writeable is True
+
+
+@pytest.mark.parametrize("as_view", [True, False])
+@given(x=tensors(read_only=True))
+def test_respects_original_writeability(x: Tensor, as_view: bool):
+    assert x.data.flags.writeable is False
+    if as_view:
+        x = x[...]
+
+    with pytest.raises(ValueError):
+        x[...] = 0
 
 
 @pytest.mark.parametrize(
@@ -71,26 +131,3 @@ def test_writing_a_view_with_a_view(
     assert_array_equal(y, dangling_view)
     assert dangling_view.base is y
     assert dangling_view.grad is None
-
-
-@pytest.mark.parametrize("constant", [True, False])
-def test_sets_and_restores_writeability(constant: bool):
-    x = mg.arange(1.0, 5.0, constant=constant)
-    y = +x
-    y[...] = 0
-    assert x.data.flags.writeable is False
-    assert y.data.flags.writeable is False
-    y.backward()
-    assert x.data.flags.writeable is True
-    assert y.data.flags.writeable is True
-
-
-@pytest.mark.parametrize("as_view", [True, False])
-@given(x=tensors(read_only=True))
-def test_respects_original_writeability(x: Tensor, as_view: bool):
-    assert x.data.flags.writeable is False
-    if as_view:
-        x = x[...]
-
-    with pytest.raises(ValueError):
-        x[...] = 0
