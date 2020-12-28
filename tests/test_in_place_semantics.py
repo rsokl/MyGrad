0e8b4340c29cfd7eb2be9fd568b062d2d2e5db27
Ryan Soklaski <ry26099@mit.edu>
2020-12-28 14:26:27 -0500

    update in-place semantics tests to include inplace-on-view cases

--- tests/test_in_place_semantics.py
+++ tests/test_in_place_semantics.py
@@ -27,6 +27,7 @@ def test_raising_during_in_place_op_doesnt_corrupt_graph(inplace_on_view: bool):
     assert w.base is y_base
     assert np.shares_memory(w, y)
     assert_allclose(w.grad, 2 * np.ones_like(y))
+    assert_allclose(y_base.grad, 2 * np.ones_like(y_base))
     assert_allclose(y.grad, 2 * np.ones_like(y))
     assert_allclose(x.grad, 4 * np.ones_like(y))
 
@@ -55,23 +56,31 @@ def test_in_place_op_propagates_to_views(constant: bool, inplace_on_view: bool):
 
 
 @given(tensors(shape=(4,), constant=False))
-def test_simple_backprop_from_view_post_upstream_mutation(x: Tensor):
-    y = +x
+@pytest.mark.parametrize("inplace_on_view", [True, False])
+def test_simple_backprop_from_view_post_upstream_mutation(
+    inplace_on_view: bool, x: Tensor
+):
+    y_base = +x
+    y = y_base[...] if inplace_on_view else y_base
     z = y[...]
     y[:2] = 0  # base is mutated
     # downstream view should carry appropriate info
     # for backprop post-mutation
     z.backward()
 
-    assert_array_equal(z, y)
-    assert_array_equal(z.grad, np.ones_like(y))
-    assert_array_equal(y.grad, np.ones_like(y))
+    assert_array_equal(y, y_base)
+    assert_array_equal(z, y_base)
+    assert_array_equal(z.grad, np.ones_like(y_base))
+    assert_array_equal(y_base.grad, np.ones_like(y_base))
+    assert_array_equal(y.grad, np.ones_like(y_base))
     assert_array_equal(x.grad, [0.0, 0.0, 1.0, 1.0])
 
 
 @given(tensors(shape=(4,), elements=st.floats(-10, 10), constant=False))
-def test_mutation_doesnt_corrupt_upstream_op(x: Tensor):
-    y = +x
+@pytest.mark.parametrize("inplace_on_view", [True, False])
+def test_mutation_doesnt_corrupt_upstream_op(inplace_on_view: bool, x: Tensor):
+    y_base = +x
+    y = y_base[...] if inplace_on_view else y_base
     view = y[...]
 
     # z = x**4
@@ -86,27 +95,32 @@ def test_mutation_doesnt_corrupt_upstream_op(x: Tensor):
     assert_allclose(z.grad, np.ones_like(y))
     assert_allclose(x.grad, 4 * np.asarray(x) ** 3)
 
+    assert y_base.grad is None
     assert y.grad is None
     assert view.grad is None
 
 
 @pytest.mark.parametrize("constant", [True, False])
-def test_sets_and_restores_writeability(constant: bool):
+@pytest.mark.parametrize("inplace_on_view", [True, False])
+def test_sets_and_restores_writeability(inplace_on_view: bool, constant: bool):
     x = mg.arange(1.0, 5.0, constant=constant)
-    y = +x
+    y_base = +x
+    y = y_base[...] if inplace_on_view else y_base
     y[...] = 0
     assert x.data.flags.writeable is False
+    assert y_base.data.flags.writeable is False
     assert y.data.flags.writeable is False
     y.backward()
     assert x.data.flags.writeable is True
+    assert y_base.data.flags.writeable is True
     assert y.data.flags.writeable is True
 
 
-@pytest.mark.parametrize("as_view", [True, False])
+@pytest.mark.parametrize("inplace_on_view", [True, False])
 @given(x=tensors(read_only=True))
-def test_respects_original_writeability(x: Tensor, as_view: bool):
+def test_respects_original_writeability(x: Tensor, inplace_on_view: bool):
     assert x.data.flags.writeable is False
-    if as_view:
+    if inplace_on_view:
         x = x[...]
 
     with pytest.raises(ValueError):
@@ -114,18 +128,24 @@ def test_respects_original_writeability(x: Tensor, as_view: bool):
 
 
 @pytest.mark.parametrize("constant", [True, False])
-def test_respects_disabled_memguard(constant: bool):
+@pytest.mark.parametrize("inplace_on_view", [True, False])
+def test_respects_disabled_memguard(constant: bool, inplace_on_view: bool):
     x = mg.arange(1.0, 5.0, constant=constant)
-    y = +x
+    y_base = +x
+    y = y_base[...] if inplace_on_view else y_base
+
     with mg.mem_guard_off:
         y[...] = 0
     assert x.data.flags.writeable is False
+    assert y_base.data.flags.writeable is True
     assert y.data.flags.writeable is True
     y.backward()
     assert x.data.flags.writeable is True
+    assert y_base.data.flags.writeable is True
     assert y.data.flags.writeable is True
 
 
+@pytest.mark.parametrize("inplace_on_view", [True, False])
 @pytest.mark.parametrize(
     "target_op",
     [
@@ -146,10 +166,12 @@ def test_respects_disabled_memguard(constant: bool):
 def test_writing_a_view_with_a_view(
     target_op: Callable[[Tensor], Tensor],
     source_op: Callable[[Tensor], Tensor],
+    inplace_on_view: bool,
     num_in_place_updates: int,
 ):
     x = mg.arange(1.0, 5.0)
-    y = +x
+    y_base = +x
+    y = y_base[...] if inplace_on_view else y_base
     dangling_view = y[...]
 
     for _ in range(num_in_place_updates):
@@ -163,16 +185,18 @@ def test_writing_a_view_with_a_view(
     ([-1, 2, -3, 4] * proxy_y).sum().backward()
 
     assert_array_equal(proxy_y.grad, [-1.0, 2.0, -3.0, 4.0])
+    assert_array_equal(y_base.grad, [-1.0, 2.0, -3.0, 4.0])
     assert_array_equal(y.grad, [-1.0, 2.0, -3.0, 4.0])
     assert_array_equal(x.grad, [0.0, 0.0, -4.0, 6.0])
 
     assert_array_equal(y, dangling_view)
-    assert dangling_view.base is y
+    assert dangling_view.base is y_base
     assert dangling_view.grad is None
 
     dangling_view.clear_graph()  # release memory
 
     assert x.data.flags.writeable
+    assert y_base.data.flags.writeable
     assert y.data.flags.writeable
     assert dangling_view.data.flags.writeable
 
