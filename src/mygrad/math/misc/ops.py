4ecc97b08a2898636355ce9f6a0f34e7c178d7f0
Ryan Soklaski <ry26099@mit.edu>
2021-02-06 18:53:59 -0500

    implement misc funcs as ufuncs

--- src/mygrad/math/misc/ops.py
+++ src/mygrad/math/misc/ops.py
@@ -1,62 +1,58 @@
 import numpy as np
 
-from mygrad.operation_base import BroadcastableOp, Operation
+from mygrad.operation_base import BinaryArith, UnaryArith
 
 __all__ = ["Abs", "Sqrt", "Cbrt", "Maximum", "Minimum"]
 
 
-class Abs(Operation):
-    def __call__(self, a):
-        self.variables = (a,)
-        return np.abs(a.data)
+class Abs(UnaryArith):
+    numpy_ufunc = np.absolute
 
     def backward_var(self, grad, index, **kwargs):
-        a = self.variables[index]
+        (a,) = self.variables
         return grad * np.piecewise(
             a.data, [a.data < 0, a.data == 0, a.data > 0], [-1, np.nan, 1]
         )
 
 
-class Sqrt(Operation):
-    def __call__(self, a):
-        """f(a) = sqrt(a)
-
-        Parameters
-        ----------
-        a : mygrad.Tensor"""
-        self.variables = (a,)
-        return np.sqrt(a.data)
+class Sqrt(UnaryArith):
+    numpy_ufunc = np.sqrt
 
     def backward_var(self, grad, index, **kwargs):
-        a = self.variables[index]
+        (a,) = self.variables
         return grad / (2 * np.sqrt(a.data))
 
 
-class Cbrt(Operation):
-    def __call__(self, a):
-        self.variables = (a,)
-        return np.cbrt(a.data)
+class Cbrt(UnaryArith):
+    numpy_ufunc = np.cbrt
 
     def backward_var(self, grad, index, **kwargs):
-        a = self.variables[index]
+        (a,) = self.variables
         return grad / (3 * np.cbrt(a.data ** 2))
 
 
-class Maximum(BroadcastableOp):
-    def __call__(self, a, b):
-        self.variables = (a, b)
-        self.greater_than_mask = a.data > b.data
-        self.equal_mask = a.data == b.data
-        return np.where(self.greater_than_mask, a.data, b.data)
+class Maximum(BinaryArith):
+    numpy_ufunc = np.maximum
+
+    def __init__(self):
+        super().__init__()
+        self.greater_than_mask = None
 
     def backward_var(self, grad, index, **kwargs):
+        a, b = self.variables
+
+        if self.greater_than_mask is None:
+            self.greater_than_mask = a.data > b.data
+
         if index == 0:
             mask = self.greater_than_mask
         elif index == 1:
+            equal_mask = a.data == b.data
             mask = np.logical_not(self.greater_than_mask)
+
             if mask.ndim:
-                np.logical_not(mask, out=mask, where=self.equal_mask)
-            elif self.equal_mask:
+                np.logical_not(mask, out=mask, where=equal_mask)
+            elif equal_mask:
                 mask = np.logical_not(mask)
         else:  # pragma: no cover
             raise IndexError(f"Back-propagation through tensor-{index}")
@@ -64,21 +60,29 @@ class Maximum(BroadcastableOp):
         return mask * grad
 
 
-class Minimum(BroadcastableOp):
-    def __call__(self, a, b):
-        self.variables = (a, b)
-        self.less_than_mask = a.data < b.data
-        self.equal_mask = a.data == b.data
-        return np.where(self.less_than_mask, a.data, b.data)
+class Minimum(BinaryArith):
+    numpy_ufunc = np.minimum
+
+    def __init__(self):
+        super().__init__()
+        self.less_than_mask = None
+        self.equal_mask = None
 
     def backward_var(self, grad, index, **kwargs):
+        a, b = self.variables
+
+        if self.less_than_mask is None:
+            self.less_than_mask = a.data < b.data
+
         if index == 0:
             mask = self.less_than_mask
         elif index == 1:
             mask = np.logical_not(self.less_than_mask)
+            equal_mask = a.data == b.data
+
             if mask.ndim:
-                np.logical_not(mask, out=mask, where=self.equal_mask)
-            elif self.equal_mask:
+                np.logical_not(mask, out=mask, where=equal_mask)
+            elif equal_mask:
                 mask = np.logical_not(mask)
         else:  # pragma: no cover
             raise IndexError(f"Back-propagation through tensor-{index}")
