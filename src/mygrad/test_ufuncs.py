05b002cf5b83e8db42c7ebb326d8a5f0c49ac415
Ryan Soklaski <ry26099@mit.edu>
2021-02-08 02:06:19 -0500

    add test for ufunc attributes

---
+++ src/mygrad/test_ufuncs.py
@@ -0,0 +1,35 @@
+import inspect
+from typing import Type
+
+import pytest
+
+from mygrad.operation_base import Ufunc
+
+
+def all_subclasses(cls):
+    return set(cls.__subclasses__()).union(
+        (s for c in cls.__subclasses__() for s in all_subclasses(c))
+    )
+
+
+all_concrete_ufuncs = [
+    ufunc for ufunc in all_subclasses(Ufunc) if not inspect.isabstract(ufunc)
+]
+
+
+@pytest.mark.parametrize("ufunc", all_concrete_ufuncs)
+@pytest.mark.parametrize(
+    "attribute",
+    [
+        "nin",
+        "nout",
+        "nargs",
+        "ntypes",
+        "types",
+        "identity",
+        "signature",
+    ],
+)
+def test_ufunc_attributes_match_numpy_counterpart(ufunc: Type[Ufunc], attribute: str):
+    ufunc = ufunc()
+    assert getattr(ufunc, attribute) == getattr(ufunc.numpy_ufunc, attribute)
