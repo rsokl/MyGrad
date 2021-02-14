40f48eff9539ae16d50aff0abd22d1a53bdbe9b0
Ryan Soklaski <ry26099@mit.edu>
2021-02-13 22:19:43 -0500

    add straggling test and restore mem-guard fixture

---
+++ tests/test_constant_inference.py
@@ -0,0 +1,31 @@
+import hypothesis.strategies as st
+import numpy as np
+from hypothesis import given
+
+import mygrad as mg
+from mygrad.math.arithmetic.ops import Multiply
+from tests import populate_args
+from tests.custom_strategies import array_likes, valid_constant_arg
+from tests.utils import clears_mem_state, expected_constant
+
+
+def mul(x, y, *, dtype=None, constant=None):
+    return mg.Tensor._op(Multiply, x, y, op_kwargs=dict(dtype=dtype), constant=constant)
+
+
+@clears_mem_state
+@given(
+    x=array_likes(shape=(1,), dtype=np.int32),
+    y=array_likes(shape=(1,), dtype=np.int32),
+    dtype=st.none() | st.just("float32") | st.just(int),
+    data=st.data(),
+)
+def test_that_typical_op_propagates_constant_under_general_conditions(
+    x, y, dtype, data: st.DataObject
+):
+    arr = np.multiply(x, y, dtype=dtype)
+    constant = data.draw(valid_constant_arg(arr.dtype), label="constant")
+    expected = expected_constant(x, y, dest_dtype=arr.dtype, constant=constant)
+
+    out = mul(x, y, **populate_args(dtype=dtype, constant=constant).kwargs)
+    assert out.constant is expected
