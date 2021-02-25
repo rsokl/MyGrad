241b5e4e105c6994abbccfcaa2775f87a595e4b1
Ryan Soklaski <ry26099@mit.edu>
2021-02-24 20:55:43 -0500

    add docs

--- tests/checkers.py
+++ tests/checkers.py
@@ -3,6 +3,8 @@ from mygrad.typing import ArrayLike
 
 
 def check_consistent_grad_dtype(*args: ArrayLike):
+    """Raises assertion error if `t.grad.dtype` does not match `t.dtype`
+    for variable tensors."""
     for item in args:
         if not isinstance(item, mg.Tensor):
             continue
