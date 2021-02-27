d56b9a79f9a3e4bec46e2f527ebb315309744b42
Ryan Soklaski <ry26099@mit.edu>
2021-02-27 13:42:59 -0500

    patch coverage of specifying out=Tensor in ufuncs

--- tests/test_where_mask.py
+++ tests/test_where_mask.py
@@ -6,23 +6,13 @@ from numpy.testing import assert_allclose, assert_array_equal
 
 import mygrad as mg
 from mygrad import Tensor
-from mygrad.math.arithmetic.ops import Multiply
-from mygrad.math.trigonometric.ops import Sin
-
-
-def mul(x, y, out=None, *, constant=None, **kwargs) -> Tensor:
-    return Tensor._op(Multiply, x, y, constant=constant, op_kwargs=kwargs, out=out)
-
-
-def sin(x, *, constant=None, **kwargs) -> Tensor:
-    return Tensor._op(Sin, x, constant=constant, op_kwargs=kwargs)
 
 
 def test_manual_multiply_no_broadcast():
     x = Tensor([1.0, 2.0, 3.0])
     y = -x.copy()
     mask = np.array([True, False, True])
-    out = mul(x, y, where=mask)
+    out = mg.multiply(x, y, where=mask)
     assert_array_equal(out[mask], [-1.0, -9.0])
     assert not np.allclose(out[~mask], -4)
     out.backward([10.0, 1.0, 20.0])
@@ -34,7 +24,7 @@ def test_manual_multiply_with_mask_broadcast():
     x = mg.arange(9.0).reshape(3, 3)
     y = -x.copy()
     mask = np.array([True, True, False])
-    out = mul(x, y, where=mask)
+    out = mg.multiply(x, y, where=mask)
     mask = np.broadcast_to(mask, (3, 3))
     assert_array_equal(out[mask].reshape(3, -1), -x.data[:, :2] ** 2)
     out.backward()
@@ -46,7 +36,7 @@ def test_manual_multiply_with_multiple_broadcast():
     x = mg.arange(9.0).reshape(3, 3)
     y = mg.tensor(-1.0)
     mask = np.array([True, True, False])
-    out = mul(x, y, where=mask)
+    out = mg.multiply(x, y, where=mask)
     mask = np.broadcast_to(mask, (3, 3))
     assert_array_equal(out[mask].reshape(3, -1), -x.data[:, :2])
     out.backward()
@@ -68,7 +58,7 @@ def test_manual_multiply_with_out(
     x = +ox
     y = -x.copy()
     mask = np.array([True, False, True])
-    out = mul(process_x(x), process_y(y), where=mask, out=process_out(x))
+    out = mg.multiply(process_x(x), process_y(y), where=mask, out=process_out(x))
 
     assert ox.data.flags.writeable is False
     assert out.data.flags.writeable is False
@@ -87,11 +77,14 @@ def test_manual_multiply_with_out(
     assert out.data.flags.writeable is True
 
 
-def test_manual_sine_no_broadcast():
-    x = mg.tensor([-mg.pi, 1.0, 0])
+@pytest.mark.parametrize("process_x", [lambda x: x, lambda x: x[...], lambda x: +x])
+@pytest.mark.parametrize("process_out", [lambda x: x, lambda x: x[...]])
+def test_unary_func_with_where_and_out(process_x, process_out):
+    ox = mg.tensor([1.0, 2.0, 3.0])
+    x = +ox
     mask = np.array([True, False, True])
-    out = sin(x, where=mask)
-    assert_allclose(out[mask], [-0, 0], atol=1e-7)
-    assert not np.allclose(out[~mask], np.sin(1.0), atol=1e-7)
-    out.backward([10.0, 1.0, 20.0])
-    assert_array_equal(x.grad, [-10.0, 0.0, 20.0])
+    mg.negative(process_x(x), where=mask, out=process_out(x))
+    assert_allclose(x, [-1.0, 2.0, -3.0], atol=1e-7)
+    x.backward([10.0, 2.0, 20.0])
+    assert_array_equal(x.grad, [10.0, 2.0, 20.0])
+    assert_array_equal(ox.grad, [-10.0, 2.0, -20.0])
