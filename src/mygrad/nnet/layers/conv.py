714da3a3700fd57a3395be09301921bd0c02412f
Ryan Soklaski <ry26099@mit.edu>
2021-02-28 13:57:04 -0500

    add plot to conv docs

--- src/mygrad/nnet/layers/conv.py
+++ src/mygrad/nnet/layers/conv.py
@@ -186,7 +186,7 @@ def conv_nd(
 
     Parameters
     ----------
-    x : Union[Tensor, array_like], shape=(N, C, Xo, ...)
+    x : ArrayLike, shape=(N, C, Xo, ...)
         The data batch to be convolved over.
 
     filter_bank : Union[Tensor, array_like], shape=(F, C, Wo, ...)
@@ -267,6 +267,27 @@ def conv_nd(
           dtype=float32)
     >>> k.grad  # d(summed_conv)/dk
     array([[[6., 6., 6., 6., 6.]]])
+
+    .. plot::
+
+       >>> import mygrad as mg
+       >>> from mygrad.nnet import conv_nd
+       >>> import matplotlib.pyplot as plt
+       >>> kernel = mg.ones(5)  # a square-wave signal
+       >>> x = mg.zeros((1, 1, 16))  # a square-wave signal
+       >>> x[..., 5:11] = 1
+       >>> k = mg.ones((1, 1, 5))    # a constant-valued kernel
+       >>> y = conv_nd(x, k, stride=1)   # performing a stride-1, 1D convolution
+       >>> plt.title("conv(f, g); stride: 1")
+       >>> y.backward()
+       >>> plt.plot(x.data[0,0], label="f", ls="--", lw=3, drawstyle='steps-pre')
+       >>> plt.plot(kernel, label="g", ls="--", lw=3, drawstyle='steps-pre')
+       >>> plt.plot(y.data[0,0], label="f * g")
+       >>> plt.plot(mg.arange(16.), x.grad[0, 0], label="d[sum(f * g)]/df")
+       >>> kernel = mg.ones(5)  # a square-wave signal
+       >>> plt.legend()
+       >>> plt.grid()
+       >>> plt.show()
 
     Now, let's demonstrate a more typical usage for ``conv_nd`` in the context of
     neural networks. ``x`` will represent 10, 32x32 RGB images, and we will use
