690c74d84c31ca22ba4e35cefb95cd72723db06d
kw-0 <46660348+kw-0@users.noreply.github.com>
2021-08-04 09:42:46 -0400

    finished save and load functions

--- src/mygrad/io.py
+++ src/mygrad/io.py
@@ -1,9 +1,10 @@
 import numpy as np
 import typing
-from mygrad.tensor_base import tensor
-from mygrad.tensor_base import Tensor
+import mygrad as mg
+from mygrad.tensor_base import tensor, Tensor
 
-def save(filename : str, arr : Tensor) -> None:
+
+def save(filename : str, tensor : Tensor) -> None:
     """Saves a tensor and its gradient information.
 
     This docstring was adapted from that of numpy.save()
@@ -11,44 +12,38 @@ def save(filename : str, arr : Tensor) -> None:
     Parameters
     ----------
     file_name : str
-        The desired name of the file that will hold the tensor data. Note that the file will be saved as a .npy
+        The desired name of the file that will hold the tensor data. Note that the file will be saved as a .npz
 
-    arr : Tensor
-        The tensor that is to be saved.
+    tensor : Tensor
+        The tensor that is to be saved, along with its gradient information. If there is no gradient, it saves as None.
 
     Returns
     -------
     None
     """
-    np.save(filename, arr.data)
+    if not isinstance(tensor, Tensor):
+        raise TypeError(f"mygrad.save requires a Tensor-type object, got type {type(tensor)}")
 
-    if arr.grad:
-        gradient_path = f'{filename}_gradient'
+    np.savez(filename, data=tensor.data, grad=tensor.grad)
 
-        np.save(gradient_path, arr.grad)
-    
-def load(tensor_filename : str, *, gradient_filename : file or str) -> Tensor:
+
+def load(tensor_filename : str) -> Tensor:
     """Loads a saved Tensor and its gradient information (if applicable).
 
-    This docstring was adapted form that of numpy.load()
+    This docstring was adapted from that of numpy.load()
 
     Parameters
     ----------
-    tensor_filename : file or str
+    tensor_filename : str
         The name of the file that holds the tensor data to load.
-
-    gradient_filename : file or str
-        The name of the file that holds the tensor's gradient data to load.
     
     Returns
     -------
     A tensor with the desired gradient data.
     """
-    loaded_tensor = tensor(np.load(tensor_filename))
-    loaded_tensor.backward(np.load(gradient_filename))
+    _tensor = np.load(tensor_filename)
+
+    loaded_tensor = tensor(_tensor['data'])
+    loaded_tensor.backward(_tensor['grad'])
 
     return loaded_tensor
-
-
-# # if it is a tensor, save as tensor, if it is a numpy array save as numpy array
-# # if it is neither, save as tensor
