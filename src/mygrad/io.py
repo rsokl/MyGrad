cfbb621e7b0469ba143081385db76df0615420b5
kw-0 <46660348+kw-0@users.noreply.github.com>
2021-08-14 21:25:00 -0400

    adds better handling of a None gradient

--- src/mygrad/io.py
+++ src/mygrad/io.py
@@ -4,14 +4,14 @@ import mygrad as mg
 from mygrad.tensor_base import tensor, Tensor
 
 
-def save(filename : str, tensor : Tensor) -> None:
+def save(file : str, tensor : Tensor) -> None:
     """Saves a tensor and its gradient information.
 
     This docstring was adapted from that of numpy.save()
 
     Parameters
     ----------
-    file_name : str
+    file : str
         The desired name of the file that will hold the tensor data. Note that the file will be saved as a .npz
 
     tensor : Tensor
@@ -24,30 +24,31 @@ def save(filename : str, tensor : Tensor) -> None:
     if not isinstance(tensor, Tensor):
         raise TypeError(f"mygrad.save requires a Tensor-type object, got type {type(tensor)}")
 
-    # if tensor.grad == None:
-    #     np.savez(filename, data=tensor.data)
-    np.savez(filename, data=tensor.data, grad=tensor.grad)
+    if tensor.grad is not None:
+        np.savez(file, data=tensor.data, grad=tensor.grad)
+    else:
+        np.savez(file, data=tensor.data)
 
 
-def load(tensor_filename : str) -> Tensor:
+def load(file : str) -> Tensor:
     """Loads a saved Tensor and its gradient information (if applicable).
 
     This docstring was adapted from that of numpy.load()
 
     Parameters
     ----------
-    tensor_filename : str
+    file : str
         The name of the file that holds the tensor data to load.
     
     Returns
     -------
     A tensor with the desired gradient data.
     """
-    _tensor = np.load(tensor_filename, allow_pickle=True)
+    _tensor = np.load(file)
 
     loaded_tensor = tensor(_tensor['data'])
 
-    if not _tensor['grad'].all():
+    if 'grad' in _tensor.files:
         loaded_tensor.backward(_tensor['grad'])
 
     return loaded_tensor
